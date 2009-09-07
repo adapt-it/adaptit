e408f0aea07916c6b8abff4cf2e668cf71a5aac2
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2009-09-07 18:57:18 +0000

    Adds a gbIsPrinting test to the code block near the end of the DrawTextRTL() routine so that it properly handles print and print preview on the various platforms.

--- trunk/source/Cell.cpp
+++ trunk/source/Cell.cpp
@@ -854,19 +854,31 @@ void CCell::DrawTextRTL(wxDC* pDC, wxString& str, wxRect& rect)
 	// indicate the current client view's total width, so if we subtract the urPt.x value
 	// from grectViewClient.GetWidth(), we should have the "mirrored" x-axis coordinate
 	// value for RTL layout.
+	// whm modified 7Sep09 The code below to account for differences that need to be taken
+	// into account when printing or print previewing
+	if (gbIsPrinting)
+	{
+		// wxGTK and wxMac need to start drawing text at the point urPt less the width/extent
+		// of the text to be drawn
+		wxSize sizeOfPhrase = pDC->GetTextExtent(str);
+		pDC->DrawText(str,urPt.x - sizeOfPhrase.x,urPt.y);
+	}
+	else
+	{
 #ifdef __WXMSW__
-	// wxMSW needs SetLayoutDirection(wxLayout_RightToLeft) to be set; in addition we
-	// need to transform the urPt so it is mirrored from the right edge of 
-	// grectViewClient.
-	wxASSERT(grectViewClient.GetWidth() >= urPt.x); // ensure grectViewClient is already set
-	pDC->SetLayoutDirection(wxLayout_RightToLeft);
-	pDC->DrawText(str,grectViewClient.GetWidth() - urPt.x - 16,urPt.y); // 16 pixels for scrollbar width
+		// wxMSW needs SetLayoutDirection(wxLayout_RightToLeft) to be set; in addition we
+		// need to transform the urPt so it is mirrored from the right edge of 
+		// grectViewClient.
+		wxASSERT(grectViewClient.GetWidth() >= urPt.x); // ensure grectViewClient is already set
+		pDC->SetLayoutDirection(wxLayout_RightToLeft);
+		pDC->DrawText(str,grectViewClient.GetWidth() - urPt.x - 16,urPt.y); // 16 pixels for scrollbar width
 #else
-	// wxGTK and wxMac need to start drawing text at the point urPt less the width/extent
-	// of the text to be drawn
-	wxSize sizeOfPhrase = pDC->GetTextExtent(str);
-	pDC->DrawText(str,urPt.x - sizeOfPhrase.x,urPt.y);
+		// wxGTK and wxMac need to start drawing text at the point urPt less the width/extent
+		// of the text to be drawn
+		wxSize sizeOfPhrase = pDC->GetTextExtent(str);
+		pDC->DrawText(str,urPt.x - sizeOfPhrase.x,urPt.y);
 #endif
+	}
 
 #ifdef __WXMSW__
 	// turning off RTL layout of the DC after calling DrawText() would seem to be necessary 
