f985778448c0da597fff692b0fc403e744bd91a6
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2010-01-05 20:54:57 +0000

    Tweaks made to the FilenameConflictFunc in wxDesigner to allow the wxTextCtrls to expand horizontally when the dialog is resized.

--- source/Adapt_It_wdr.cpp
+++ source/Adapt_It_wdr.cpp
@@ -6853,7 +6853,7 @@ wxSizer *FilenameConflictFunc( wxWindow *parent, bool call_fit, bool set_sizer )
     item6->Add( item7, 0, wxALIGN_CENTER|wxALL, 5 );
 
     wxTextCtrl *item8 = new wxTextCtrl( parent, ID_TEXTCTRL_SOURCE_FILE_DETAILS, wxT(""), wxDefaultPosition, wxSize(200,64), wxTE_MULTILINE|wxNO_BORDER | wxGROW | wxTE_NO_VSCROLL );
-    item6->Add( item8, 0, wxALIGN_CENTER|wxALL, 5 );
+    item6->Add( item8, 1, wxALIGN_CENTER|wxALL, 5 );
 
     item4->Add( item6, 1, wxGROW|wxALIGN_CENTER_VERTICAL|wxLEFT|wxRIGHT|wxBOTTOM, 5 );
 
@@ -6870,7 +6870,7 @@ wxSizer *FilenameConflictFunc( wxWindow *parent, bool call_fit, bool set_sizer )
     item11->Add( item12, 0, wxALIGN_CENTER|wxALL, 5 );
 
     wxTextCtrl *item13 = new wxTextCtrl( parent, ID_TEXTCTRL_DESTINATION_FILE_DETAILS, wxT(""), wxDefaultPosition, wxSize(200,64), wxTE_MULTILINE|wxNO_BORDER | wxGROW | wxTE_NO_VSCROLL );
-    item11->Add( item13, 0, wxALIGN_CENTER|wxALL, 5 );
+    item11->Add( item13, 1, wxALIGN_CENTER|wxALL, 5 );
 
     item9->Add( item11, 1, wxGROW|wxALIGN_CENTER_VERTICAL|wxLEFT|wxRIGHT|wxBOTTOM, 5 );
 
