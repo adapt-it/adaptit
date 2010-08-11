c7f265daec4042f81596e8c7881bcd5f36bad5ea
adaptit.bruce@gmail.com <adaptit.bruce@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2010-08-11 06:57:57 +0000

    My preferred icon choice

--- branches/ai_codefixes/source/Adapt_It.cpp
+++ branches/ai_codefixes/source/Adapt_It.cpp
@@ -6440,16 +6440,16 @@ bool CAdapt_ItApp::OnInit() // MFC calls this InitInstance()
 	// To evaluate four different frame icon choices:
 	//    Define only one of the four choices below to select it for use as 16x16 frame icon.
 	//    Comment out the other three choices.
-	//#define IconBlueShadesOn16x16WhiteRectangularBackground
-	#define IconBlueShadesOn16x16WhiteDiskBackground
+	#define IconBlueShadesOn16x16WhiteRectangularBackground
+	//#define IconBlueShadesOn16x16WhiteDiskBackground
 	//#define IconBlueShadesOn16x16AllTransparentBackground
 	//#define IconWhiteBlueOn16x16BlueDiskBackground
 
 	// To evaluate four different taskbar icon choices:
 	//    Define only one of the four choices to select it for use as 32x32 taskbar icon.
 	//    Comment out the other three choices.
-	//#define IconBlueShadesOn32x32WhiteRectangularBackground
-	#define IconBlueShadesOn32x32WhiteDiskBackground
+	#define IconBlueShadesOn32x32WhiteRectangularBackground
+	//#define IconBlueShadesOn32x32WhiteDiskBackground
 	//#define IconBlueShadesOn32x32AllTransparentBackground
 	//#define IconWhiteBlueOn32x32BlueDiskBackground
 
