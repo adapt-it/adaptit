f239ad6602e9e7644184f1ff6375c256c82ab2e2
adaptit.bruce@gmail.com <adaptit.bruce@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2011-05-12 07:10:23 +0000

    The new feature 'Import Edited Source Text' is finished and tested and works well. Code is in a new pair of files: MergeUpdatedSrc.h and .cpp. The guts of the functionality is encapsulated in a single function, MergeUpdatedSourceText(). Under the hood, the SPLists involved are converted to SPArrays so that indices can be used (for simpler code); the code involves recursion, looking for 'longest span of in-common CSourcePhrase instances' each time. The functionality will also be used in the Paratext collaboration feature. Designed & coded by BEW in April & early May of 2011.

--- source/helpers.cpp
+++ source/helpers.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
 /// \project		adaptit
-/// \file			MergeUpdatedSrc.cpp
+/// \file			helpers.cpp
 /// \author			Bruce Waters, revised for wxWidgets by Bill Martin
 /// \date_created	6 January 2005
 /// \date_revised	20 July 2010
