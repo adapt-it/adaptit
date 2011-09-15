45727cc74b520829ed89c45417828836eaa97c3c
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2011-09-15 23:31:30 +0000

    Adds a #define called _USE_HTML_FILE_VIEWER to beginning of Adapt_It.h that can be temporarily uncommented to test the CHtmlFileViewer for reading the "Help for Administrators.htm" file (instead of the usual call of the default browser to do it).

--- trunk/source/Adapt_It.h
+++ trunk/source/Adapt_It.h
@@ -95,6 +95,11 @@ class NavProtectNewDoc; // for user navigation protection feature
 // interface).
 #define _EXCHANGE_DATA_DIRECTLY_WITH_BIBLEDIT 0
 
+// Use 1 to force the use of the CHtmlFileViewer for testing purposes instead of viewing the 
+// Help for Administrators.htm file within the default browser. Set to 0 for normal execution
+// and distribution of the Application.
+#define _USE_HTML_FILE_VIEWER
+
 // uncomment to turn on wxLogDebug tracking of gnBeginInsertionsSequNum & gnEndInsertionsSequNum
 //#define Highlighting_Bug
 
