49d0cb4fb4278cb13ef4f15c6b28d1fa2569a4f7
kevinlbradford@gmail.com <kevinlbradford@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2011-09-24 20:28:51 +0000

    Print Glosses. Includes new subclass of wxPreviewFrame

---
+++ source/AIPrintPreviewFrame.cpp
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			AIPrintPreviewFrame.cpp
+/// \author			Kevin Bradford
+/// \date_created	23 September 2011
+/// \date_revised	
+/// \copyright		2008 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General 
+///                 Public License (see license directory)
+/// \description	This is the implementation file for the CAIPrintPreviewFrame class. 
+/// The CAIPrintPreviewFrame class is the derived from wxPreviewFrame. 
+/// It allows control of the underlying frame/window
+/// during the print preview process.
+/// \derivation		The CAIPrintPreviewFrame class is derived from wxPreviewFrame.
+/////////////////////////////////////////////////////////////////////////////
+#include "AIPrintPreviewFrame.h"
+#include "Adapt_It.h"
+#include "Adapt_ItView.h"
+
+CAIPrintPreviewFrame::CAIPrintPreviewFrame(
+	CAdapt_ItView * view,
+	wxPrintPreviewBase *  preview,  
+	wxWindow *  parent,  
+	const wxString &  title,  
+	const wxPoint &  pos,  
+	const wxSize &  size,  
+	long  style,  
+	const wxString &  name )
+	: wxPreviewFrame( preview, parent, title, pos, size, style, name) 
+{
+	pView = view;
+	wxASSERT(pView != NULL);
+	bHideGlossesOnClose = FALSE;
+}
+
+CAIPrintPreviewFrame::~CAIPrintPreviewFrame(void)
+{
+	if (bHideGlossesOnClose	 == TRUE)
+		pView->ShowGlosses();
+	//wxPreviewFrame::~wxPreviewFrame();
+}
+
+// Setting this to true will redraw the underlying application view
+//    before closing the window, allowing us to show glosses and repaint
+//    before exiting
+void CAIPrintPreviewFrame::HideGlossesOnClose( bool bClose )
+{ 
+	bHideGlossesOnClose = bClose;
+}
---
+++ source/AIPrintPreviewFrame.h
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			AIPrintPreviewFrame.h
+/// \author			Kevin Bradford
+/// \date_created	23 September 2011
+/// \date_revised	
+/// \copyright		2008 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General 
+///                 Public License (see license directory)
+/// \description	This is the header file for the CAIPrintPreviewFrame class. 
+/// The CAIPrintPreviewFrame class is the derived from wxPreviewFrame. 
+/// It allows control of the underlying frame/window
+/// during the print preview process.
+/// \derivation		The CAIPrintPreviewFrame class is derived from wxPreviewFrame.
+/////////////////////////////////////////////////////////////////////////////
+#include "c:\wxwidgets-2.8.11\include\wx\prntbase.h"
+
+// forward declarations
+class CAdapt_ItView;
+
+class CAIPrintPreviewFrame :
+	public wxPreviewFrame
+{
+public:
+	CAIPrintPreviewFrame(
+		CAdapt_ItView * view,
+		wxPrintPreviewBase *  preview,  
+		wxWindow *  parent,  
+		const wxString &  title = "Print Preview",  
+		const wxPoint &  pos = wxDefaultPosition,  
+		const wxSize &  size = wxDefaultSize,  
+		long  style = wxDEFAULT_FRAME_STYLE,  
+		const wxString &  name = wxFrameNameStr);
+	~CAIPrintPreviewFrame(void);
+
+	void HideGlossesOnClose( bool );
+
+private:
+	CAdapt_ItView* pView;
+	bool bHideGlossesOnClose;
+
+
+};
