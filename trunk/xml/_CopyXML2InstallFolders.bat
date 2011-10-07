63459340e149414f013b0bd3545db0b3fc84a314
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2011-10-07 22:18:41 +0000

    Adds the 5 Paratext dlls needed by rdwrtp7.exe to the xml folder, the same folder where rdwrtp7.exe exists in the repository. The _CopyXML2InstallFolders.bat file will copy these to the appropriate "setup Regular ..." and "setup Unicode ..." folders in preparation for inclusion in the installters by Inno Setup.

--- trunk/xml/_CopyXML2InstallFolders.bat
+++ trunk/xml/_CopyXML2InstallFolders.bat
@@ -13,6 +13,12 @@ echo AI_USFM.xml is copied to the above installation folders
 echo books.xml is copied to the above installation folders
 echo AI_UserProfiles.xml is copied to the above installation folders
 echo rdwrtp7.exe is copied to the above installation folders
+echo along with the following 5 Paratext dll files:
+echo    ParatextShared.dll
+echo    NetLoc.dll 
+echo    Interop.XceedZipLib.dll
+echo    ICSharpCode.SharpZipLib.dll
+echo    Utilities.dll
 echo ===================================================
 echo Press CRTL-C to abort or
 pause
@@ -25,6 +31,11 @@ copy "AI_USFM.xml" "..\setup Regular\"
 copy "books.xml" "..\setup Regular\"
 copy "AI_UserProfiles.xml" "..\setup Regular\"
 copy "rdwrtp7.exe" "..\setup Regular\"
+copy "ParatextShared.dll" "..\setup Regular\"
+copy "NetLoc.dll" "..\setup Regular\"
+copy "Interop.XceedZipLib.dll" "..\setup Regular\"
+copy "ICSharpCode.SharpZipLib.dll" "..\setup Regular\"
+copy "Utilities.dll" "..\setup Regular\"
 
 @echo on
 rem 2. The following copies Documentation files to the "setup Regular - Minimal" folder
@@ -34,6 +45,11 @@ copy "AI_USFM.xml" "..\setup Regular - Minimal\"
 copy "books.xml" "..\setup Regular - Minimal\"
 copy "AI_UserProfiles.xml" "..\setup Regular - Minimal\"
 copy "rdwrtp7.exe" "..\setup Regular - Minimal\"
+copy "ParatextShared.dll" "..\setup Regular - Minimal\"
+copy "NetLoc.dll" "..\setup Regular - Minimal\"
+copy "Interop.XceedZipLib.dll" "..\setup Regular - Minimal\"
+copy "ICSharpCode.SharpZipLib.dll" "..\setup Regular - Minimal\"
+copy "Utilities.dll" "..\setup Regular - Minimal\"
 
 @echo on
 rem 3. The following copies Documentation files to the "setup Regular - No Html Help" folder
@@ -43,6 +59,11 @@ copy "AI_USFM.xml" "..\setup Regular - No Html Help\"
 copy "books.xml" "..\setup Regular - No Html Help\"
 copy "AI_UserProfiles.xml" "..\setup Regular - No Html Help\"
 copy "rdwrtp7.exe" "..\setup Regular - No Html Help\"
+copy "ParatextShared.dll" "..\setup Regular - No Html Help\"
+copy "NetLoc.dll" "..\setup Regular - No Html Help\"
+copy "Interop.XceedZipLib.dll" "..\setup Regular - No Html Help\"
+copy "ICSharpCode.SharpZipLib.dll" "..\setup Regular - No Html Help\"
+copy "Utilities.dll" "..\setup Regular - No Html Help\"
 
 @echo on
 rem 4. The following copies Documentation files to the "setup Unicode" folder
@@ -52,6 +73,11 @@ copy "AI_USFM.xml" "..\setup Unicode\"
 copy "books.xml" "..\setup Unicode\"
 copy "AI_UserProfiles.xml" "..\setup Unicode\"
 copy "rdwrtp7.exe" "..\setup Unicode\"
+copy "ParatextShared.dll" "..\setup Unicode\"
+copy "NetLoc.dll" "..\setup Unicode\"
+copy "Interop.XceedZipLib.dll" "..\setup Unicode\"
+copy "ICSharpCode.SharpZipLib.dll" "..\setup Unicode\"
+copy "Utilities.dll" "..\setup Unicode\"
 
 @echo on
 rem 5. The following copies Documentation files to the "setup Unicode - Minimal" folder
@@ -61,6 +87,11 @@ copy "AI_USFM.xml" "..\setup Unicode - Minimal\"
 copy "books.xml" "..\setup Unicode - Minimal\"
 copy "AI_UserProfiles.xml" "..\setup Unicode - Minimal\"
 copy "rdwrtp7.exe" "..\setup Unicode - Minimal\"
+copy "ParatextShared.dll" "..\setup Unicode - Minimal\"
+copy "NetLoc.dll" "..\setup Unicode - Minimal\"
+copy "Interop.XceedZipLib.dll" "..\setup Unicode - Minimal\"
+copy "ICSharpCode.SharpZipLib.dll" "..\setup Unicode - Minimal\"
+copy "Utilities.dll" "..\setup Unicode - Minimal\"
 
 @echo on
 rem 6. The following copies Documentation files to the "setup Unicode - No Html Help" folder
@@ -70,6 +101,11 @@ copy "AI_USFM.xml" "..\setup Unicode - No Html Help\"
 copy "books.xml" "..\setup Unicode - No Html Help\"
 copy "AI_UserProfiles.xml" "..\setup Unicode - No Html Help\"
 copy "rdwrtp7.exe" "..\setup Unicode - No Html Help\"
+copy "ParatextShared.dll" "..\setup Unicode - No Html Help\"
+copy "NetLoc.dll" "..\setup Unicode - No Html Help\"
+copy "Interop.XceedZipLib.dll" "..\setup Unicode - No Html Help\"
+copy "ICSharpCode.SharpZipLib.dll" "..\setup Unicode - No Html Help\"
+copy "Utilities.dll" "..\setup Unicode - No Html Help\"
 
 echo ===================================================
 echo Copy process completed.
