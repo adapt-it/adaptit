ad464f9cd6ccc5a326e1d94eb1952be629974446
adaptit.bruce@gmail.com <adaptit.bruce@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2011-11-29 01:35:07 +0000

    Linux printing feature finished. Tested. Appears to be now robust. Some errors in Win and Mac printing fixed too. A bit more checking of Win printing needed to be sure.

--- source/Adapt_ItView.h
+++ source/Adapt_ItView.h
@@ -243,7 +243,7 @@ public:
     bool        DoRangePrintOp(const int nBeginSequNum, const int nEndSequNum,
                                wxPrintData* pPrintData); // BEW created 14Nov11
 #if defined(__WXGTK__)
-    bool        SetupPageRangePrintOp(const int nFromPage, const int nToPage, wxPrintData* pPrintData);
+    bool        SetupPageRangePrintOp(const int nFromPage, int nToPage, wxPrintData* pPrintData);
 #endif
     // BEW 14Nov11, don't know why Bill has WXUNUSED() here, his code uses these params, so I've removed the WXUNUSED()
 	//bool		SetupRangePrintOp(const int nFromCh, const int nFromV, const int nToCh,
