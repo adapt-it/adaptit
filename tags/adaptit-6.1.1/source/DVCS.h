bc7e71622ae40bc88c502b37be9b3b8274788eac
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2012-01-09 03:56:32 +0000

    Tagging for 6.1.1

---
+++ tags/adaptit-6.1.1/source/DVCS.h
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			DVCS.cpp
+/// \author			Mike Hore
+/// \date_created	22 December 2011
+/// \date_revised
+/// \copyright		2008 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General Public
+///                 License (see license directory)
+/// \description
+/////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef DVCS_h
+#define DVCS_h
+
+// the following improves GCC compilation performance
+#if defined(__GNUG__) && !defined(__APPLE__)
+    #pragma interface "DocPage.h"
+#endif
+
+void  CallHg();
+
+#endif /* DVCS_h */
