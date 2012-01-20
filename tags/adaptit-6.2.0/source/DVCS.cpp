b67345e0aff2f37c07be18255b8ac7229dcfcc1a
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2012-01-20 21:11:26 +0000

    Tagging for 6.2.0

---
+++ tags/adaptit-6.2.0/source/DVCS.cpp
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			DVCS.cpp
+/// \author			Mike Hore
+/// \date_created	22 December 2011
+/// \date_revised
+/// \copyright		2008 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General Public
+///                 License (see license directory)
+/// \description This is the implementation file for
+/////////////////////////////////////////////////////////////////////////////
+
+// the following improves GCC compilation performance
+#if defined(__GNUG__) && !defined(__APPLE__)
+    #pragma implementation "DVCS.h"
+#endif
+
+// For compilers that support precompilation, includes "wx.h".
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+// Include your minimal set of headers here, or wx.h
+#include <wx/wx.h>
+#endif
+
+// other includes
+#include <wx/docview.h> // needed for classes that reference wxView or wxDocument
+#include <wx/valgen.h> // for wxGenericValidator
+#include <wx/wizard.h>
+#include <wx/filesys.h> // for wxFileName
+
+#include "Adapt_It.h"
+#include "MainFrm.h"
+#include "Adapt_ItDoc.h"
+#include "Adapt_ItView.h"
+#include "Adapt_ItCanvas.h"
+#include "ProjectPage.h"
+#include "LanguagesPage.h"
+#include "FontPage.h"
+#include "PunctCorrespPage.h"
+#include "CaseEquivPage.h"
+#include "UsfmFilterPage.h"
+#include "DocPage.h"
+#include "scrollingwizard.h" // whm added 13Nov11 for wxScrollingWizard - need to include this here before "StartWorkingWizard.h" below
+#include "StartWorkingWizard.h"
+//#include "SourceBundle.h"
+#include "Pile.h"
+#include "Layout.h"
+#include "WhichBook.h"
+#include "helpers.h"
+
+
+#if defined(TEST_CHORUS)
+
+void  CallHg()
+{
+    wxMessageBox(_T("here we go!!!!"));
+}
+
+#endif
