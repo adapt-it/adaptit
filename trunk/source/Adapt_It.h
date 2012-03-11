e96f596eeea4b95f728c7c03fa9732c4e309bc5d
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2012-03-11 22:48:53 +0000

    Set release date for 6.2.0 to 14 Mar 2012.

--- trunk/source/Adapt_It.h
+++ trunk/source/Adapt_It.h
@@ -99,7 +99,7 @@ class NavProtectNewDoc; // for user navigation protection feature
 #define VERSION_MINOR_PART 2
 #define VERSION_BUILD_PART 0
 #define PRE_RELEASE 0  // set to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
-#define VERSION_DATE_DAY 10
+#define VERSION_DATE_DAY 14
 #define VERSION_DATE_MONTH 3
 #define VERSION_DATE_YEAR 2012
 const wxString appVerStr(_T("6.2.0"));
