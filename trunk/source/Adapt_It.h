c349c9bc769642ceb5865226c0deb623cd66088e
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2012-08-08 15:35:07 +0000

    Changed date of the next 6.2.3 release tentatively to 24 August 2012.

--- trunk/source/Adapt_It.h
+++ trunk/source/Adapt_It.h
@@ -137,8 +137,8 @@ class NavProtectNewDoc; // for user navigation protection feature
 #define VERSION_MINOR_PART 2
 #define VERSION_BUILD_PART 3
 #define PRE_RELEASE 0  // set to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
-#define VERSION_DATE_DAY 27
-#define VERSION_DATE_MONTH 7
+#define VERSION_DATE_DAY 24
+#define VERSION_DATE_MONTH 8
 #define VERSION_DATE_YEAR 2012
 const wxString appVerStr(_T("6.2.3"));
 
