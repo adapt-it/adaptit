aa7a47f1adf457511227fc1a70ce68e1a8b2b45f
ebrommers@gmail.com <ebrommers@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2012-11-09 20:47:48 +0000

    Add LastChangedVersion keyword to version info files.

--- source/Adapt_It.h
+++ source/Adapt_It.h
@@ -148,11 +148,12 @@ class KbServer;
 #define VERSION_MAJOR_PART 6
 #define VERSION_MINOR_PART 4
 #define VERSION_BUILD_PART 0
+#define VERSION_REVISION_PART $LastChangedRevision$
 #define PRE_RELEASE 0  // set to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
 #define VERSION_DATE_DAY 30
 #define VERSION_DATE_MONTH 11
 #define VERSION_DATE_YEAR 2012
-const wxString appVerStr(_T("6.4.0"));
+const wxString appVerStr(_T("6.4.0.$LastChangedRevision$"));
 
 //#define Print_failure
 
