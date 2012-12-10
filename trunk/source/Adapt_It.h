fe3104c29fcf99558e8893eb9712573fffa380ce
ebrommers@gmail.com <ebrommers@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2012-12-10 18:01:47 +0000

    Update release date for 6.4.0 (12/12/12).

--- trunk/source/Adapt_It.h
+++ trunk/source/Adapt_It.h
@@ -157,8 +157,8 @@ class KbServer;
 #define VERSION_BUILD_PART 0
 #define VERSION_REVISION_PART 0
 #define PRE_RELEASE 0  // set to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
-#define VERSION_DATE_DAY 30
-#define VERSION_DATE_MONTH 11
+#define VERSION_DATE_DAY 12
+#define VERSION_DATE_MONTH 12
 #define VERSION_DATE_YEAR 2012
 const wxString appVerStr(_T("6.4.0"));
 const wxString svnVerStr(_T("$LastChangedRevision$"));
