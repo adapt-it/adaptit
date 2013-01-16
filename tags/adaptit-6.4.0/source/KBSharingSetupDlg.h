7ff4e82f40586283a7002ee150f67d12b9924c86
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2013-01-16 19:06:59 +0000

    Tagging 6.4.0

---
+++ tags/adaptit-6.4.0/source/KBSharingSetupDlg.h
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			KBSharingSetup.h
+/// \author			Bruce Waters
+/// \date_created	15 January 2013
+/// \rcs_id $Id: KBSharingSetup.h 3028 2013-01-15 11:38:00Z jmarsden6@gmail.com $
+/// \copyright		2013 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General Public License (see license directory)
+/// \description	This is the header file for the KBSharing class.
+/// The KBSharing class provides a dialog for the turning on or off KB Sharing, and for
+/// controlling the non-automatic functionalities within the KB sharing feature.
+/// \derivation		The KBSharing class is derived from AIModalDialog.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef KBSharingSetupDlg_h
+#define KBSharingSetupDlg_h
+
+// the following improves GCC compilation performance
+#if defined(__GNUG__) && !defined(__APPLE__)
+    #pragma interface "KBSharingSetupDlg.h"
+#endif
+
+#if defined(_KBSERVER)
+
+class KBSharingSetupDlg : public AIModalDialog
+{
+public:
+	KBSharingSetupDlg(wxWindow* parent); // constructor
+	virtual ~KBSharingSetupDlg(void); // destructor
+
+	// other methods
+
+protected:
+	void InitDialog(wxInitDialogEvent& WXUNUSED(event));
+	void OnOK(wxCommandEvent& event);
+	void OnCancel(wxCommandEvent& event);
+
+	//void OnBtnGetAll(wxCommandEvent& WXUNUSED(event));
+
+	// Note: I've stored the to-be-typed-just-once kb server password in the CMainFrame
+	// instance, and the dialog for getting the user to type it in is there too, and a
+	// private member wxString, m_kbserverPassword which keeps the value for the session's
+	// duration. When either the adapting or glossing KbServer instance needs the password
+	// it can get it from there with the accessor: GetKBSvrPassword()
+
+	DECLARE_EVENT_TABLE()
+};
+
+#endif
+
+#endif /* KBSharingSetupDlg_h */
---
+++ tags/adaptit-6.4.0/source/conschk_exists_notu_dlg.h
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			conschk_exists_notu_dlg.h
+/// \author			Bruce Waters
+/// \date_created	1 Sepember 2011
+/// \rcs_id $Id$
+/// \copyright		2011 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General Public License (see license directory)
+/// \description	This is the header file for the conschk_exists_notu_dlg class. 
+/// The conschk_exists_notu_dlg class provides an "inconsistency found" dialog which the
+/// user employs for for fixing a KB-Document inconsistency. Deals with the document pile
+/// having pSrcPhrase with m_bHasKBEntry (or m_bHasGlossingKBEntry if the current mode is
+/// glossing mode) TRUE, but KB lookup failed to find a CTargetUnit for the source text at
+/// this location in the document, and there is an existing adaptation. 
+/// (We won't need this for glossing mode, because the <Not In KB> is not available there,
+/// and so the fix is fully determinate and can be done without showing any dialog)
+/// \derivation		The conschk_exists_notu_dlg class is derived from AIModalDialog.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef conschk_exists_notu_dlg_h
+#define conschk_exists_notu_dlg_h
+
+// the following improves GCC compilation performance
+#if defined(__GNUG__) && !defined(__APPLE__)
+    #pragma interface "conschk_exists_notu_dlg.h"
+#endif
+
+/// The conschk_exists_notu_dlg class provides an "inconsistency found" dialog which the
+/// user employs for for fixing a KB-Document inconsistency. Deals with the document pile
+/// having pSrcPhrase with m_bHasKBEntry (or m_bHasGlossingKBEntry if the current mode is
+/// glossing mode) TRUE, but KB lookup failed to find a CTargetUnit for the source text at
+/// this location in the document, and there is an existing adaptation. 
+/// (We won't need this for glossing mode, because the <Not In KB> is not available there,
+/// and so the fix is fully determinate and can be done without showing any dialog)
+class conschk_exists_notu_dlg : public AIModalDialog
+{
+public:
+	conschk_exists_notu_dlg(
+		wxWindow* parent,
+		wxString* title,
+		int msgNumber,
+		wxString* srcPhrase, // for pSrcPhrase->m_key value
+		wxString* adaptation, // for pSrcPhrase->m_adaption value
+		wxString* notInKBStr, // for <Not In KB>, that is, strNotInKB
+		bool      bShowCentered); // constructor
+	virtual ~conschk_exists_notu_dlg(); // destructor
+
+	// member variables
+	int			   m_msgNumber;
+	wxTextCtrl*    m_pTextCtrlSrcText;
+	wxTextCtrl*	   m_pTextCtrlTgtText;
+	wxStaticText*  m_pStaticCtrl;
+	wxString	   m_radioNotInKBLabelStr;
+	wxRadioButton* m_pStoreNormallyRadioBtn;
+	wxRadioButton* m_pNotInKBRadioBtn;
+	wxCheckBox*	   m_pAutoFixChkBox;
+	// the following store the creation passed-in param values
+	wxString		m_sourcePhrase; // store *srcPhrase
+	wxString		m_targetPhrase; // store *adaptation
+	wxString		m_modeWordPlusArticle; // for modeWordAdaptPlusArticle
+	wxString		m_notInKBStr; // for <Not In KB>, that is, strNotInKB
+	bool			m_bDoAutoFix; // for the Auto-fix checkbox value
+	wxSizer*		pConsChk_exists_notu_dlgSizer; // not really needed
+	enum FixItAction actionTaken;
+	bool			m_bShowItCentered;
+	wxPoint			m_ptBoxTopLeft;
+	int				m_nTwoLineDepth;
+	wxStaticText*	m_pStatTxtMsg;
+
+protected:
+	void InitDialog(wxInitDialogEvent& WXUNUSED(event));
+	void OnOK(wxCommandEvent& event);
+
+private:
+	// class attributes
+	
+	// other class attributes
+
+	DECLARE_EVENT_TABLE()
+};
+#endif /* conschk_exists_notu_dlg_h */
