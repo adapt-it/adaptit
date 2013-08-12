acfc662885e3a4ccd3288953c63877b082bba7e7
ebrommers@gmail.com <ebrommers@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2013-08-12 18:01:08 +0000

    TeamCity build -- update build-ai.sh to build UnicodeDebug for linux continuous builds.

--- scripts/build-ai.sh
+++ scripts/build-ai.sh
@@ -1,59 +1,13 @@
 #!/bin/bash
-# build-ai.sh -- extracts 3pt libs and builds AdaptIt on Ubuntu
-# Currently this only builds the UnicodeDebug release against wx2.9.4,
-# but the commented out blocks will build the Unicode Release target.
+# build-ai.sh -- builds AdaptIt on Ubuntu
+# Currently this only builds the UnicodeDebug release for the continuous builds
+# on TeamCity.
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 TRUNK=$DIR/../
-VENDOR=$DIR/../../vendor
-WXGTK=$VENDOR/wxwidgets/current/wxWidgets
 
 # remove files from previous builds
-rm -rf $WXGTK
 rm -rf $TRUNK/bin/linux/UnicodeDebug
-
-# check for 7za
-#if ! type "7za" > /dev/null; then
-#  sudo apt-get -y install p7zip-full
-#fi
-# check for dos2unix
-#if ! type "dos2unix" > /dev/null; then
-#  sudo apt-get -y install dos2unix
-#fi
-# some distros use tofrodos - install it if the dos2unix package isn't there
-# and create a symlink (alias) so we can use dos2unix
-#if ! type "dos2unix" > /dev/null; then
-#    sudo apt-get -y install tofrodos
-#    sudo ln -s fromdos dos2unix
-#fi
-
-# Extract wxWidgets from the vendor branch
-unzip -a -o -q $VENDOR/wxwidgets/current/wxWidgets-2.9.4.zip -d$WXGTK
-#7za x -o$WXGTK -y $VENDOR/wxwidgets/current/wxWidgets-2.9.4.7z
-# change the line endings from Windows (CRLF) to Linux
-#find $WXGTK -type f -exec dos2unix {} \; 
-# copy over the appropriate setup.h so wxwidgets knows which platform to build
-cp -f $WXGTK/include/wx/gtk/setup.h $WXGTK/include/wx/setup.h
-# allow a couple wxwidgets scripts to execute
-chmod 755 $WXGTK/configure
-chmod 755 $WXGTK/src/stc/gen_iface.py
-
-# Build wxWidgets (debug)
-cd $WXGTK
-mkdir -p buildgtkud
-# release --> requires buildgtku directory to be made
-# (cd $WXGTK/buildgtku && $WXGTK/configure --with-gtk --enable-unicode --with-gnomeprint && make)
-#if [ $? -ne 0 ]
-#then
-#  echo "Error building wxWidgets Unicode Release: $?"
-#  exit $?
-#fi
-(cd $WXGTK/buildgtkud && $WXGTK/configure --with-gtk --enable-unicode --enable-debug --with-gnomeprint && make)
-if [ $? -ne 0 ]
-then
-  echo "Error building wxWidgets Unicode Debug: $?"
-  exit $?
-fi
 
 # Configure svn in adaptit-standard way
 # (just in case it isn't already set up like this)
@@ -63,16 +17,10 @@ do
   sed -i -e "s/^# \*\.$x = .*$/*.$x = svn:eol-style=CRLF/" ~/.subversion/config
 done
 
-# Build adaptit (debug)
+# Build adaptit (UnicodeDebug) and return the results
 cd $TRUNK/bin/linux/
 mkdir -p UnicodeDebug
-#(cd $TRUNK/bin/linux/Unicode && ../configure --with-wx-config=$WXGTK/buildgtku/wx-config && make)
-#if [ $? -ne 0 ]
-#then
-#  echo "Error building Adapt It Unicode Release: $?"
-#  exit $?
-#fi
-(cd $TRUNK/bin/linux/UnicodeDebug && ../configure --with-wx-config=$WXGTK/buildgtkud/wx-config && make)
+(cd $TRUNK/bin/linux/UnicodeDebug && ../configure --enable-debug && make)
 if [ $? -ne 0 ]
 then
   echo "Error building Adapt It Unicode Debug: $?"
