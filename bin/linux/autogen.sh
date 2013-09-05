16bc74cf399b472f3e11b656c25de2b401fb80e5
ebrommers@gmail.com <ebrommers@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2013-09-05 16:07:32 +0000

    TeamCity linux build -- remove -verbose flag from aclocal call in autogen.sh

--- bin/linux/autogen.sh
+++ bin/linux/autogen.sh
@@ -3,7 +3,7 @@ LIBTOOLIZE=libtoolize
 if [ "`uname`" = "Darwin" ] ; then
    LIBTOOLIZE=glibtoolize
 fi
-aclocal -verbose -I m4
+aclocal -I m4
 $LIBTOOLIZE --automake --force --copy
 #libtoolize --verbose --automake --force --copy
 automake -v -a -c -Wno-portability
