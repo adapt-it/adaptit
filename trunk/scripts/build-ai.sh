dea9f56fea38afd41e819619744d7ed594573229
ebrommers@gmail.com <ebrommers@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2013-09-05 21:13:04 +0000

    TeamCity linux build - back out temporary changes (no difference in build failure)

--- trunk/scripts/build-ai.sh
+++ trunk/scripts/build-ai.sh
@@ -12,8 +12,8 @@ echo "Base source control directory: $TRUNK"
 echo "-------------------------------------"
 
 # troubleshooting helps
-#set -e
-#set -x
+set -e
+set -x
 
 # remove files from previous builds
 rm -rf $TRUNK/bin/linux/UnicodeDebug
@@ -26,24 +26,24 @@ fi
 # Build adaptit (UnicodeDebug) and return the results
 cd $TRUNK/bin/linux
 # make sure the old configure and friends are gone
-#rm -f Makefile.in configure config.sub config.guess aclocal.m4 ltmain.sh
+rm -f Makefile.in configure config.sub config.guess aclocal.m4 ltmain.sh
 # call autogen to generate configure and friends
 ./autogen.sh
-#bin/linux/autogen.sh
-#if [ $? -ne 0 ]
-#then
-#  echo "Error in autogen.sh script: $?"
-#  exit 1
-#fi
+bin/linux/autogen.sh
+if [ $? -ne 0 ]
+then
+  echo "Error in autogen.sh script: $?"
+  exit 1
+fi
 
 # create the Unicode Debug directory and call configure
 mkdir -p UnicodeDebug
 (cd UnicodeDebug && ../configure --enable-debug)
-#if [ $? -ne 0 ]
-#then
-#  echo "Error configuring for UnicodeDebug build: $?"
-#  exit 1
-#fi
+if [ $? -ne 0 ]
+then
+  echo "Error configuring for UnicodeDebug build: $?"
+  exit 1
+fi
 
 #($CONFIG/UnicodeDebug/make)
 (cd UnicodeDebug && make)
