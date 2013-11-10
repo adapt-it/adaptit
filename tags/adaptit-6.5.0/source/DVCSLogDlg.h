b8f19eec3784239647195868152e445a336e7643
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2013-11-10 23:55:40 +0000

    Tagging 6.5.0

---
+++ tags/adaptit-6.5.0/source/DVCSLogDlg.h
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			DVCSNavDlg.cpp
+/// \author			Mike Hore
+/// \date_created	25 March 2013
+/// \rcs_id $Id:
+/// \copyright		2008 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General Public
+///                 License (see license directory)
+/// \description
+/////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef DVCSLogDlg_h
+#define DVCSLogDlg_h
+
+// the following improves GCC compilation performance
+#if defined(__GNUG__) && !defined(__APPLE__)
+    #pragma interface "DVCSLogDlg.h"
+#endif
+
+class DVCSLogDlg : public AIModalDialog
+{
+public:
+    DVCSLogDlg (wxWindow*  parent);         // constructor
+    virtual ~DVCSLogDlg (void);             // destructor
+    
+    CAdapt_ItApp*   m_pApp;
+    CAdapt_ItDoc*   m_pDoc;
+    wxSizer*        m_dlgSizer;
+    wxListView*     m_pList;
+
+    void InitDialog (void);
+
+protected:
+
+private:
+    void PopulateList();
+
+	DECLARE_EVENT_TABLE()
+};
+
+
+#endif /* DVCSLogDlg_h */
