8116c9ba8dc1b992101b2ac43ed7e51da25f0553
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2013-11-13 15:29:42 +0000

    Tagging 6.5.1

---
+++ tags/adaptit-6.5.1/source/CollabUtilities.h
@@ -0,0 +1,267 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			CollabUtilities.h
+/// \author			Bruce Waters, from code taken from SetupEditorCollaboration.h by Bill Martin
+/// \date_created	27 July 2011
+/// \rcs_id $Id$
+/// \copyright		2011 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General Public License (see license directory)
+/// \description	This is a header file containing some utility functions used by Adapt It's
+///                 collaboration feature - collaborating with either Paratext or Bibledit. 
+/////////////////////////////////////////////////////////////////////////////
+//
+#ifndef collabUtilities_h
+#define collabUtilities_h
+
+// the following improves GCC compilation performance
+#if defined(__GNUG__) && !defined(__APPLE__)
+    #pragma interface "CollabUtilities.h"
+#endif
+
+#ifndef _string_h_loaded
+#define _string_h_loaded
+#include "string.h"
+#endif
+#include "Adapt_It.h"
+#include "helpers.h"
+
+// This createNewProjectInstead is used in GetAIProjectFolderForCollab(),
+// GetSourceTextFromEditor.cpp, and SetupEditorCollaboration.cpp.
+const wxString createNewProjectInstead = _("<Create a new project instead>");
+
+struct VerseAnalysis {
+	bool				bIsComplex; // TRUE if verse num is a range, or something like 6b,
+								// or is a gap in the chapter/verses; set to FALSE if 
+								// just a simple number
+	wxString			strDelimiter; // delimiter string used in a range, eg. - in 3-5
+	wxString			strStartingVerse; // string version of the verse number or first verse number of a range
+	wxChar				charStartingVerseSuffix;  // for the a in something like 6a-8, or 9a
+	wxString			strEndingVerse; // string version of the verse number or final verse number of a range
+	wxChar				charEndingVerseSuffix; // for the a in something like 15-17a
+};
+/// An enum for selecting which kind of text to generate in order to send it back to the external
+/// editor (PT or BE), whether target text, or a free translation
+enum SendBackTextType
+{
+	makeTargetText = 1,
+	makeFreeTransText
+};
+enum DoFor 
+{
+	collab_source_text,
+	collab_target_text,
+	collab_freeTrans_text
+};
+enum CommandLineFor
+{
+	reading,
+	writing
+};
+// this struct stores an index to a "\\mkr :nnn:md5sum" line in a structure&extents array
+// and also stores, in startOffset and endOffset, indices to the first and last wxChar
+// instances of the span of text within the wxString which gave rise to the
+// structure&extents array
+struct MD5Map {
+	int md5Index;
+	size_t startOffset;
+	size_t endOffset;
+};
+
+// This struct is used for finding the next sync position when the main loop comes to
+// mismatched markers (which not necessarily are \v markers, we need to deal with things
+// like \q mismatched with \c etc). We also want to handle \c like we do for \v, so that
+// for purposes of the matching algorithm when we come to a "complex" part of the array
+// matchups, we can halt at the \c which precedes the \v 1, and treat the \c as equivalent
+// to a verse marker with verse number 0. So for a halt at \c, we put verseNumStr =
+// _T("0") and set chapterStr to the actual number string for the chapter, and bIsComplex
+// to FALSE; but for a halt at a verse, chapterStr will be empty, and verseNumStr will
+// have something different than _T("0"), and bIsComplex could be TRUE or FALSE depending
+// on what is in verseNumStr)
+struct VerseInf {
+	wxString verseNumStr;
+	wxString chapterStr;
+	int indexIntoArray;
+	bool bIsComplex;
+};
+
+
+
+
+class CBString;
+class SPList;	// declared in SourcePhrase.h WX_DECLARE_LIST(CSourcePhrase, SPList); macro 
+				// and defined in SourcePhrase.cpp WX_DEFINE_LIST(SPList); macro
+class CSourcePhrase;
+
+///////////////////////////////////////////////////////////////////////////////////
+/// This group of functions are used for analysis of texts in order to get an updated text
+/// to return to the external editor
+///////////////////////////////////////////////////////////////////////////////////
+	enum			EditorProjectVerseContent DoProjectAnalysis(enum CollabTextType textType,
+						wxString compositeProjName,wxString editor,
+						wxString& emptyBooks,wxString& booksWithContent,wxString& errorMsg);
+	wxArrayString	BreakStringBufIntoChapters(const wxString& bookString);
+	bool			DoVerseAnalysis(const wxString& verseNum, VerseAnalysis& rVerseAnal); // return TRUE if is complex
+	// this overload takes an array of structure& extent checksums, gets the
+	// wxString at the 0-based index given by lineIndex, and fills out the passed in
+	// VerseAnalysis struct; returns TRUE if the verse structure is complex, FALSE if it
+	// is simple
+	bool			DoVerseAnalysis(VerseAnalysis& refVAnal, const wxArrayString& md5Array, size_t lineIndex);
+	void			DeleteAllVerseInfStructs(wxArrayPtrVoid& arr);
+	void			DelineateComplexChunksAssociation(const wxArrayString& postEditMd5Arr, 
+							const wxArrayString& fromEditorMd5Arr, int postEditStart, 
+							int& postEditEnd, int fromEditorStart, int& fromEditorEnd);
+	bool			GetNextVerseLine(const wxArrayString& usfmText, int& index);
+	bool			GetAnotherVerseOrChapterLine(const wxArrayString& usfmText, int& index, wxString& chapterStr);
+	wxString		GetInitialUsfmMarkerFromStructExtentString(const wxString str);
+	wxString		GetStrictUsfmMarkerFromStructExtentString(const wxString str);
+	wxString		GetFinalMD5FromStructExtentString(const wxString str);
+	int				GetCharCountFromStructExtentString(const wxString str);
+	wxString		GetNumberFromChapterOrVerseStr(const wxString& verseStr);
+	void			GetRemainingMd5VerseLines(const wxArrayString& md5Arr, int nStart, 
+												wxArrayPtrVoid& verseLinesArr);
+	void GetChapterListAndVerseStatusFromBook(enum CollabTextType textType, 
+								wxArrayString& usfmStructureAndExtentArray,
+								wxString collabCompositeProjectName,
+								wxString targetBookFullName, 
+								wxArrayString& staticBoxDescriptionArray,
+								wxArrayString& chapterList,
+								wxArrayString& statusList,
+								bool& bBookIsEmpty);
+	//void GetChapterListAndVerseStatusFromTargetBook(wxString targetBookFullName, 
+	//							wxArrayString& chapterList, wxArrayString& statusList);
+	wxString GetStatusOfChapter(enum CollabTextType cTextType, wxString collabCompositeProjectName,
+		const wxArrayString &usfmStructureAndExtentArray, int indexOfChItem, wxString bookFullName,
+		bool& bChapterIsEmpty, wxString& nonDraftedVerses);
+	int				FindExactVerseNum(const wxArrayString& md5Arr, int nStart, const wxString& verseNum);
+	int				FindMatchingVerseNumInOtherArray(const wxArrayPtrVoid& verseInfArr, wxString& verseNum,
+												wxString chapterStr);
+	int				FindNextChapterLine(const wxArrayString& md5Arr, int nStartAt, bool& bBeforeChapterOne);
+	void			InitializeVerseAnalysis(VerseAnalysis& rVerseAnal);
+	bool			IsChapterLine(const wxArrayString& usfmText, int index, wxString& chapterStr);
+	bool			IsVerseLine(const wxArrayString& usfmText, int index);
+
+	bool			IsComplexVersificationLine(const wxArrayString& md5Arr, size_t lineIndex);
+	bool			IsComplexVersificationLine(const wxString& verseLineStr);
+
+	// The next subset are the main workhorses for the creation of the updated text...
+	
+	// Next function includes the nStart & nFinish items within the subarray
+	wxArrayString	ObtainSubarray(const wxArrayString arr, size_t nStart, size_t nFinish); 
+	// BEW added 11July, to get changes to the adaptation and free translation back to the
+	// respective PT or BE projects; the post-edit text from the document at the time of
+	// File / Save is returned in the postEditText parameter - it will be either target
+	// text, or freeTrans text, and the caller will use it to replace what is stored in
+	// the relevant app member for the pre-edit tgt or freeTrans text. The returned
+	// wxString is the updated USFM text string to be sent to PT or BE - or an empty
+	// string if there was an error.
+	wxString		MakeUpdatedTextForExternalEditor(SPList* pDocList, 
+							enum SendBackTextType makeTextType, wxString& postEditText);
+	bool			OpenDocWithMerger(CAdapt_ItApp* pApp, wxString& pathToDoc, wxString& newSrcText, 
+						    bool bDoMerger, bool bDoLayout, bool bCopySourceWanted);
+	wxString		GetUpdatedText_UsfmsUnchanged(wxString& postEditText, wxString& fromEditorText,
+							wxArrayString& preEditMd5Arr, wxArrayString& postEditMd5Arr, 
+							wxArrayString& fromEditorMd5Arr,wxArrayPtrVoid& postEditOffsetsArr, 
+							wxArrayPtrVoid& fromEditorOffsetsArr);
+	wxString		GetUpdatedText_UsfmsChanged(wxString& preEditText, wxString& postEditText, 
+							wxString& fromEditorText, wxArrayString& preEditMd5Arr, 
+							wxArrayString& postEditMd5Arr, wxArrayString& fromEditorMd5Arr, 
+							wxArrayPtrVoid& postEditOffsetsArr, wxArrayPtrVoid& fromEditorOffsetsArr);
+	////////////////// end of those for analysis of texts //////////////////////////
+
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	///   Functions for connecting  the externally obtained data into a new or existing
+	///   Adapt It project, and getting data out of the Adapt It document
+	/////////////////////////////////////////////////////////////////////////////////////
+	bool			CollabProjectIsEditable(wxString projShortName);
+	bool			CreateNewAIProject(CAdapt_ItApp* pApp, wxString& srcLangName, 
+							wxString& tgtLangName, wxString& srcEthnologueCode, 
+							wxString& tgtEthnologueCode, bool bDisableBookMode);
+	wxString		ExportTargetText_For_Collab(SPList* pDocList);
+	wxString		ExportFreeTransText_For_Collab(SPList* pDocList);
+	wxString		GetAIProjectFolderForCollab(wxString& aiProjName, wxString& aiSrcLangName, 
+						wxString& aiTgtLangName, wxString editorSrcProjStr, wxString editorTgtProjStr);
+	// The next function is created from OnWizardPageChanging() in Projectpage.cpp, and
+	// tweaked so as to remove support for the latter's context of a wizard dialog
+	bool			HookUpToExistingAIProject(CAdapt_ItApp* pApp, wxString* pProjectName, 
+							wxString* pProjectFolderPath);
+	bool			IsEthnologueCodeValid(wxString& code);
+	bool			CollabProjectFoundInListOfEditorProjects(wxString projName, wxArrayString projList,wxString& composedProjStr); // whm added 19Apr12
+	bool			KeepSpaceBeforeEOLforVerseMkr(wxChar* pChar); //BEW added 13Jun11
+	// a module for doing the layout and getting the view ready for the user to start
+	// adapting;; it is not limited to being used in a Collaboration scenario
+	void			SetupLayoutAndView(CAdapt_ItApp* pApp, wxString& docTitle);
+	void			UnloadKBs(CAdapt_ItApp* pApp);
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	///     Functions for MD5 checksums and their manipulation
+	/////////////////////////////////////////////////////////////////////////////////////
+	enum			CompareUsfmTexts CompareUsfmTextStructureAndExtent(const wxArrayString& usfmText1, 
+							const wxArrayString& usfmText2);
+	void			DeleteMD5MapStructs(wxArrayPtrVoid& structsArr);
+	wxArrayString	GetUsfmStructureAndExtent(wxString& sourceFileBuffer);
+	bool			IsTextOrPunctsChanged(wxString& oldText, wxString& newText);
+	// overload of the above, taking arrays and start & finish item indices as parameters
+	bool			IsTextOrPunctsChanged(wxArrayString& oldMd5Arr, int oldStart, int oldEnd,
+							wxArrayString& newMd5Arr, int newStart, int newEnd);
+	bool			IsUsfmStructureChanged(wxString& oldText, wxString& newText);
+	void			MapMd5ArrayToItsText(wxString& itsText, wxArrayPtrVoid& mappingsArr, 
+							wxArrayString& md5Arr);
+
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	///     Functions for filename creation, path creation, file moving, and data transfer
+	///     to the external editor, when doing collaboration
+	/////////////////////////////////////////////////////////////////////////////////////
+	bool			BookExistsInCollabProject(wxString projCompositeName, wxString bookFullName);
+	bool			CollabProjectHasAtLeastOneBook(wxString projCompositeName,wxString collabEditor);
+	bool			CollabProjectsAreValid(wxString srcCompositeProjName, wxString tgtCompositeProjName, 
+							wxString frtrCompositeProjName, wxString collabEditor,
+							wxString& errorStr, wxString& errorProjects);
+	wxString		GetPathToRdwrtp7(); // used in GetSourceTextFromEditor::OnInit() and CollabUtilities.cpp
+	wxString		GetPathToBeRdwrt(); // used in GetSourceTextFromEditor::OnInit() and CollabUtilities.cpp
+	wxString		GetBibleditInstallPath();  // used in GetSourceTextFromEditor::OnInit()
+	wxString		GetTextFromAbsolutePathAndRemoveBOM(wxString& absPath, wxString bookCodeForID);
+	wxString		GetTextFromFileInFolder(CAdapt_ItApp* pApp, wxString folderPath, wxString& fileTitle);
+	wxString		GetTextFromFileInFolder(wxString folderPathAndName); // an override of above function
+	wxString		GetShortNameFromProjectName(wxString projName);
+	//wxString		GetFullNameFromProjectName(wxString projName); // unused
+	wxString		GetLanguageNameFromProjectName(wxString projName);
+	void			GetAILangNamesFromAIProjectNames(const wxString aiProjectName, wxString& sourceLang, wxString& targetLang);
+	// The next three are the meaty ones, which together get the updated text back to the
+	// external editor, via the .temp folder (child of the work folder) as an
+	// intermediatory location for storage of the file holding the data being transferred
+	// in or out as the case may be - determined by the lineFor value, either 'reading' or
+	// 'writing'
+	wxString		MakePathToFileInTempFolder_For_Collab(enum DoFor textKind);
+	wxString		BuildCommandLineFor(enum CommandLineFor lineFor, enum DoFor textKind);
+	void			TransferTextBetweenAdaptItAndExternalEditor(enum CommandLineFor lineFor, enum DoFor textKind,
+							wxArrayString& textIOArray, wxArrayString& errorsIOArray, long& resultCode);
+	// Move the newSrc string of just-obtained (from PT or BE) source text, currently in the
+	// .temp folder, to the __SOURCE_INPUTS folder, creating the latter folder if it doesn't
+	// already exist, and storing in a file with filename constructed from fileTitle plus an
+	// added .txt extension; if a file of that name already exists there, overwrite it.
+	bool			MoveTextToFolderAndSave(CAdapt_ItApp* pApp, wxString& folderPath, 
+							wxString& pathCreationErrors, wxString& theText, wxString& fileTitle,
+							bool bAddBOM = FALSE);
+	// The following is similar to MoveTextToFolderAndSave() except that it computes
+	// internally the path to the .temp folder, and adds the required filename to it (also
+	// internally) and so fewer parameters are needed in the signature
+	bool			MoveTextToTempFolderAndSave(enum DoFor textKind, wxString& theText, bool bAddBOM = FALSE);
+	// The next two move text to or from Bibledit's native storage structures
+	bool			CopyTextFromBibleditDataToTempFolder(wxString projectPath, wxString bookName, 
+							int chapterNumber, wxString tempFilePathName, wxArrayString& errors);
+	bool			CopyTextFromTempFolderToBibleditData(wxString projectPath, wxString bookName, 
+							int chapterNumber, wxString tempFilePathName, wxArrayString& errors);
+	// Returns the absolute path to the folder being used as the Adapt It work folder, whether
+	// in standard location or in a custom location - but for the custom location only
+	// provided it is a "locked" custom location (if not locked, then the path to the standard
+	// location is returned, i.e. m_workFolderPath, rather than m_customWorkFolderPath). The
+	// "lock" condition ensures that a snooper can't set up a PT or BE collaboration and
+	// the remote user not being aware of it.
+	wxString		SetWorkFolderPath_For_Collaboration();
+	wxString		ChangeFilenameExtension(wxString filenameOrPath, wxString extn);
+	wxString		RemoveIDMarkerAndCode(wxString textForSending);
+
+#endif
+
---
+++ tags/adaptit-6.5.1/source/Stack.cpp
@@ -0,0 +1,198 @@
+/////////////////////////////////////////////////////////////////////////////
+/// \project		adaptit
+/// \file			stack.cpp
+/// \author			Bruce Waters; modified by Bill Martin for the WX version
+/// \date_created	October 2004
+/// \rcs_id $Id$
+/// \copyright		2008 Bruce Waters, Bill Martin, SIL International
+/// \license		The Common Public License or The GNU Lesser General Public License (see license directory)
+/// \description	This is the implementation file for the CStack class.
+/// Note: my CStack class, the XML parser, and my string class CBString
+/// will all be single-byte characters. When used with the Uncode supporting
+/// app, these single-byte characters will be interpretted as UTF-8, and the
+/// callback functions which the XML parser uses will internally convert the
+/// byte string to UTF-16; and so the signature of these callbacks has to have
+/// a wxString so as to accept the UTF-16 for a unicode build. Elsewhere
+/// in the XML module, where strings are required, I will use my CBString class.
+/// \derivation		The CStack class is not a derived class.
+/////////////////////////////////////////////////////////////////////////////
+
+// **** The following conditional compile directives added for wxWidgets support
+#if defined(__GNUG__) && !defined(__APPLE__)
+    #pragma implementation "Stack.h"
+#endif
+
+// For compilers that support precompilation, includes "wx.h".
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+// Include your minimal set of headers here, or wx.h
+#include <wx/wx.h>
+#endif
+
+#include "Adapt_It.h"
+#include "Stack.h"
+
+CStack::CStack()
+{
+	top = 0;
+	memset((void*)items,0,MAX*32);
+}
+
+bool CStack::IsEmpty() const
+{
+	return top == 0;
+}
+
+bool CStack::IsFull() const
+{
+	return top == MAX;
+}
+
+bool CStack::Push(Item item)
+{
+	if (top < MAX)
+	{
+		strcpy(items[top++],item);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool CStack::Pop(Item item)
+{
+	if (top > 0)
+	{
+		strcpy(item,items[--top]);
+		memset(items[top],0,32);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void CStack::GetTop(Item item)
+{
+	if (top > 0)
+	{
+		strcpy(item,items[--top]);
+		top++;
+	}
+	else
+	{
+		int i;
+		for (i=0; i<32; i++)
+		{
+			item[i] = '\0';
+		}
+	}
+}
+
+bool CStack::Contains(const Item item)
+{
+	if (top > 0)
+	{
+		int i;
+		for (i = 0; i < top; i++) // BEW 5Jun10, < not <= as top is index of next 
+								  // fillable cell (& it doesn't have any data in it yet)
+		{
+			if (strcmp(item,items[i]) == 0)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// checks from 1 to 3 parents when an element is nested, starting from the one immediately
+// under the top element, and comparing nThisMany - up to a maximum of 3 parents; return
+// true if the ones checked are matches for what is on the stack; pass an empty string for
+// any which are not to be checked - fill the parameters from the left in reverse order of
+// the nesting in the xml file, item_up1 (being the first parent) must always have something
+// in it if the function result is to have any useful meaning; the second parent (the more
+// 'outer' tag of the nesting) would be item_up2. Usually that's all we require. For
+// flexibility a third parameter is provided, for a more 'outer' still tag, but our LIFT
+// support will not need to test any more than two parents. The tags tested can be at any
+// depth in the stack - since the testing starts at the tag one-down from the topmost item
+// in the stack. Note: the top index for the stack indexes not the top element, but the
+// empty cell immediately beyond that. So if the cell with index 5 has a tagname in it,
+// the the value of top would be 6 (stack indices start from 0).
+// So if we have the following:
+// <sense>
+//     <definition>
+//         <text>somePCDATA</text>
+// Then the "current" top item on the stack is the one which we presumably are processing
+// using, say, an AtLIFTPCDATA() or similar callback, the first parent will be <definition>
+// and the second parent will be <sense> -- so we need to test only for two parents here, 
+// though there may well be other tags higher up which we don't test for.
+// Any arguments unused for testing in the function should have emptyStr as their parameter,
+// as defined above..
+// So we would make the statements:
+// static char emptyStr[32];  // near the .cpp file's start
+// void* r = memset((void*)emptyStr,0,32); // after the above line
+// bool bMatched = pStack->MyParentsAre(2,xml_definition,xml_sense,emptyStr); // in AtXXXyyy()
+// Note, the arguments from left to right correspond to moving back though nested xml tags
+// starting from the one above the current one.
+// 
+bool CStack::MyParentsAre(int nThisMany,const Item item_up1, const Item item_up2, 
+						  const Item item_up3)
+{
+	int index;
+	int start = top - 2;
+	int end = start - nThisMany + 1;
+	if (nThisMany > 3) return false; // too many checks requested
+	if (nThisMany >= top) return false; // can't make all the requested checks
+	int counter = -1;
+	// next 3 lines for debugging - comment out when verified robust
+	//CBString parent1;
+	//CBString parent2;
+	//CBString parent3;
+	for (index = start; index >= end; index--)
+	{
+		++counter;
+		switch (counter)
+		{
+		case 0:
+			{
+				/* for debugging */ //parent1 = items[index];
+				if (strcmp(item_up1,items[index]) != 0)
+					return false;
+			}
+			break;
+		case 1:
+			{
+				/* for debugging */ //parent2 = items[index];
+				if (item_up2[0] == '\0')
+					return true;
+				else if (strcmp(item_up2,items[index]) != 0)
+					return false;
+			}
+			break;
+		case 2:
+			{
+				/* for debugging */ //parent3 = items[index];
+				if (item_up3[0] == '\0')
+					return true;
+				else if (strcmp(item_up3,items[index]) != 0)
+					return false;
+			}
+			break;
+		}
+	}
+	return true;
+}
+
