2140f5868247adea9d066ab84a21ecc1c5ad4e5b
adaptitbill@gmail.com <adaptitbill@gmail.com@2da0e052-7446-0410-92c2-8b9fcb9ac2a4>
2013-12-13 22:17:41 +0000

    Update the repository copy of feedback.php to reflect its current form on the adapt-it.org server.

--- php/feedback.php
+++ php/feedback.php
@@ -3,6 +3,9 @@
 // Spam filters will not allow delivery. Instead, choose one or more addresses in the adapt-it.org domain and
 // either check mail directly on those additional accounts or use RPOP to get the mail.
 
+// Revised 6Dec2013 by whm to better delineate literal strings using ' and embedded strings with \". Also isolates
+// the eol chars as "\n" or "\n\n" following dot.
+
 $recipient = "support@adapt-it.org";
 //$recipient = "developers@adapt-it.org";
 //$recipient = "bill_martin@adapt-it.org";
@@ -10,73 +13,73 @@ $recipient = "support@adapt-it.org";
 
 if (empty($_POST)) {
 
-    // We only accpet POSTs
+    // We only accept POSTs
     header('HTTP/1.0 403 Forbidden');
     exit;
 
 } else {
 
     $subject = "[Adapt It ";
-    $subject .= $_POST['reporttype']."] ";
+    $subject .= $_POST['reporttype'] . "] ";
     $subject .= $_POST['emailsubject'];
     
-    $random_hash = md5(date('r', time())); 
+    $random_hash = md5(date('r', time()));
+    $mime_boundary = "AdaptIt--x{$random_hash}x"; 
     $notify_log = "Log is Attached:";
     $attach_log = $_POST['attachlog'];
     $notify_doc = "Packed Document Is Attached:";
     $attach_doc = $_POST['attachdoc'];
     
     $headers = "From: ";
-    $headers .= "Adapt It <robot@adapt-it.org>\r\n";
-    $headers .= "Reply-To: ";
-    $headers .= $_POST['senderemailaddr']."\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Adapt It <robot@ebible.org>" . "\n";
+    $headers .= "Reply-To: " . $_POST['sendername'] . " <" . $_POST['senderemailaddr'] . ">" . "\n";
+    $headers .= "MIME-Version: 1.0" . "\n";
     
     if ($attach_log == $notify_log || $attach_doc == $notify_doc)
     {
         // There is at least one attachment so use multipart/mixed and MIME boundary
-        $headers .= "Content-Type: multipart/mixed; boundary=\"Adapt It--{$random_hash}\"\r\n\r\n"; 
+        $headers .= "Content-Type: multipart/mixed; boundary=\"{$mime_boundary}\"" . "\n\n"; 
         // Note: The boundary string behavior is described in RFC1341 part 7.2.1
-        // If boundary="simple boundary", then each part should begin with "--simple boundary\r\n"
-        // with the last "Content-... declarations line ending with double CRLFs \r\n\r\n. The final
-        // boundary is of the form "--simple boundary--\r\n"
-        $message .= "This is a multi-part message in MIME format.\r\n";
-        $message .= "--Adapt It--{$random_hash}\r\n";
-        $message .= "Content-Type: text/plain; charset=\"iso-8859-1\"; format=flowed\r\n";
-        $message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+        // If boundary="simple boundary", then each part should begin with "--simple boundary\n"
+        // with the last "Content-... declarations line ending with double CRLFs \n\n. The final
+        // boundary is of the form "--simple boundary--\n"
+        $message .= "This is a multi-part message in MIME format." . "\n";
+        $message .= "--{$mime_boundary}" . "\n";
+        $message .= "Content-Type: text/plain; charset=\"iso-8859-1\"; format=\"flowed\"" . "\n";
+        $message .= "Content-Transfer-Encoding: 7bit" . "\n\n";
     }
     
     // build the message body
-    $message .= "Submitted at ".date("F j, Y, g:i a")."\r\n";
-    $message .= "To: Adapt It Developers <support@adapt-it.org>\r\n";
+    $message .= "Submitted at " . date("F j, Y, g:i a") . "\n";
+    $message .= "To: Adapt It Developers <support@adapt-it.org>" . "\n";
     $message .= "Name of Sender: ";
-    $message .= $_POST['sendername']."\r\n";
+    $message .= $_POST['sendername'] . "\n";
     $message .= "Email address of Sender: ";
-    $message .= $_POST['senderemailaddr']."\r\n";
+    $message .= $_POST['senderemailaddr'] . "\n";
     if ($attach_log == $notify_log)
     {
         // Add plain text note in email body that "Log Is Attached:"
-        $message .= "      ".$notify_log."\r\n";
+        $message .= "      " . $notify_log . "\n";
     }
     if ($attach_doc == $notify_doc)
     {
         // Add plain text note in email body that "Packed Document Is Attached:"
-        $message .= "      ".$notify_doc."\r\n";
+        $message .= "      " . $notify_doc . "\n";
     }
     
-    $message .= "\r\n".$_POST['reporttype'].":\r\n";
-    $message .= $_POST['emailbody']."\r\n\r\n";
+    $message .= "\n" . $_POST['reporttype'] . ":" . "\n";
+    $message .= $_POST['emailbody'] . "\n\n";
     
-    $message .= "System Information:\r\n";
-    $message .= $_POST['sysinfo']."\r\n\r\n";;
+    $message .= "System Information:" . "\n";
+    $message .= $_POST['sysinfo'] . "\n\n";
     
     if ($attach_log == $notify_log)
     {
         // the userlog is to be attached so build it
-        $message .= "--Adapt It--{$random_hash}\r\n";
-        $message .= "Content-Type: application/octet-stream; name=\"userlog.zip\"\r\n"; 
-        $message .= "Content-Transfer-Encoding: base64\r\n";
-        $message .= "Content-Disposition: attachment; filename=\"userlog.zip\"\r\n\r\n";
+        $message .= "--{$mime_boundary}" . "\n";
+        $message .= "Content-Type: application/octet-stream; name=\"userlog.zip\"" . "\n"; 
+        $message .= "Content-Transfer-Encoding: base64" . "\n";
+        $message .= "Content-Disposition: attachment; filename=\"userlog.zip\"" . "\n\n";
         // Note: userlog below was already encoded for base64 by Adapt It before posting
         $attachment_log = chunk_split($_POST['userlog']);
         $message .= $attachment_log;
@@ -85,10 +88,10 @@ if (empty($_POST)) {
     if ($attach_doc == $notify_doc)
     {
         // the packed document is to be attached so build it
-        $message .= "--Adapt It--{$random_hash}\r\n";
-        $message .= "Content-Type: application/octet-stream; name=\"packeddoc.aip\"\r\n"; 
-        $message .= "Content-Transfer-Encoding: base64\r\n";
-        $message .= "Content-Disposition: attachment; filename=\"packeddoc.aip\"\r\n\r\n";
+        $message .= "--{$mime_boundary}" . "\n";
+        $message .= "Content-Type: application/octet-stream; name=\"packeddoc.aip\"" . "\n"; 
+        $message .= 'Content-Transfer-Encoding: base64' . "\n";
+        $message .= "Content-Disposition: attachment; filename=\"packeddoc.aip\"" . "\n\n";
         // Note: packdoc below was already encoded for base64 by Adapt It before posting
         $attachment_doc = chunk_split($_POST['packdoc']);
         $message .= $attachment_doc;
@@ -97,7 +100,7 @@ if (empty($_POST)) {
     if ($attach_log == $notify_log || $attach_doc == $notify_doc)
     {
       // Final boundary, but only if there were attachments
-      $message .= "--Adapt It--{$random_hash}--\r\n";
+      $message .= "--{$mime_boundary}--" . "\n";
     }
 
     // Send message to email address
