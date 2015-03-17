6fb7399e245578171800797822ff1cc664e8649a
eb1 <ebrommers@gmail.com>
2015-03-17 09:25:40 -0500

    Convert svn to git in linux scripts

--- scripts/check-source-eol-style.sh
+++ scripts/check-source-eol-style.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 # check-source-eol-style.sh -- checks all source files have svn:eol-style set to native
 
-SVNURL=http://adaptit.googlecode.com/svn/trunk/source	# Where's the code?
-LOCALSVNBASEDIR=~/subversion				# Where to work with svn locally
+GITURL=https://github.com/adapt-it/adaptit.git	# Where's the code?
+LOCALBASEDIR=~/git				# Where to work with code locally
 LOCALREPO=$(basename $0 .sh)				# Directory name of local copy
 SOURCEFILEREGEX='\.c$|\.cpp$|\.h$'			# Which files are source files?
 
 # Create or update local svn checkout
-if [ -d "${LOCALSVNBASEDIR}/${LOCALREPO}" ]; then
+if [ -d "${LOCALBASEDIR}/${LOCALREPO}" ]; then
   # Update existing checked out copy
-  cd ${LOCALSVNBASEDIR}/${LOCALREPO}
-  svn -q update
+  cd ${LOCALBASEDIR}/${LOCALREPO}
+  git pull
 else
   # Grab a fresh copy
-  cd ${LOCALSVNBASEDIR}
-  svn -q co ${SVNURL} ${LOCALREPO}
+  cd ${LOCALBASEDIR}
+  git clone ${GITURL} ${LOCALREPO}
 fi
 
 # Check each source file
-cd ${LOCALSVNBASEDIR}/${LOCALREPO}
-svn ls -R . | (while read i
-do
-  [ -f "$i" ] || continue 			# Ignore unless a file
-  [[ "$i" =~ ${SOURCEFILEREGEX} ]] || continue	# Ignore unless a source file
-  props=$(svn -q proplist "$i" )		# Does it have properties
-  if [ -z "$props" ]; then
+cd ${LOCALBASEDIR}/${LOCALREPO}
+#svn ls -R . | (while read i
+#do
+#  [ -f "$i" ] || continue 			# Ignore unless a file
+#  [[ "$i" =~ ${SOURCEFILEREGEX} ]] || continue	# Ignore unless a source file
+#  props=$(svn -q proplist "$i" )		# Does it have properties
+#  if [ -z "$props" ]; then
     # Alert that it is missing
-    echo WARNING: $SVNURL/$i lacks eol-style. Fix with svn propset svn:eol-style native $i
-  else
-    if [[ "$props" =~ .*svn:eol-style$ ]]; then
+#    echo WARNING: $SVNURL/$i lacks eol-style. Fix with svn propset svn:eol-style native $i
+#  else
+#    if [[ "$props" =~ .*svn:eol-style$ ]]; then
       # Get the svn:eol-style property value
-      eolstyle=$(svn propget svn:eol-style $i)
+#      eolstyle=$(svn propget svn:eol-style $i)
       # Alert if it is not native
-      [ "$eolstyle" == "native" ] || echo ERROR: $SVNURL/$i has eol-style $eolstyle. Fix with svn propset svn:eol-style native $i
-    fi
-  fi
-done)
+#      [ "$eolstyle" == "native" ] || echo ERROR: $SVNURL/$i has eol-style $eolstyle. Fix with svn propset svn:eol-style native $i
+#    fi
+#  fi
+#done)
