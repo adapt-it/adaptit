c9a7d8bdce80ba72a0b57879877167ac05c07aab
pngbill <bill_martin@sil.org>
2017-01-27 22:38:39 -0600

    Fix path spec for building on local machine.

--- scripts/makewx.sh
+++ scripts/makewx.sh
@@ -10,7 +10,7 @@
 # -- Builds are configured using the --without-gtkprint flag to force the library to use 
 #    Adobe PostScript 2.0 instead of PostScript 3.0 for printing to PostScript printers.
 # Author: Bill Martin <bill_martin@sil.org>
-# Date: 2017-01-26
+# Date: 2017-01-27
 # A sudo call should be avoided since this script is designed to be called from within CodeBlocks 
 # as a Pre-build script where user interaction is not feasible.
 # Requires an Internet connection to use wget if the wxWidgets archive is needed but doesn't 
@@ -19,7 +19,6 @@
 
 WXVER="3.0.2"
 WXDIRNAME="wxWidgets-$WXVER"
-WXDIR="$HOME/$WXDIRNAME"
 RELEASEBUILDDIR="/buildgtku"
 DEBUGBUILDDIR="/buildgtkud"
 WXGZARCHIVE="wxWidgets-$WXVER.tar.bz2"
@@ -29,7 +28,7 @@ DEBUGCONFIGOPTIONS="--enable-static --disable-shared --enable-monolithic --witho
 RELEASECONFIGOPTIONS="--enable-static --disable-shared --enable-monolithic --without-gtkprint"
 BUILDS=""
 ABORT="FALSE"
-WXSOURCETREE=$WXDIR  # Parameter $2 may change this location/path to other than $WXDIR
+WXBASEDIR=$HOME  # Parameter $2 may change this location/path to other than $HOME
 
 # The following variable definitions are unused except when testing checkinstall
 MAINTAINER="bill_martin@sil.org"
@@ -40,7 +39,7 @@ RELEASE="1"
 LICENSE="wxWindows"
 GROUP="checkinstall"
 ARCHITECTURE="amd64"
-SOURCELOCATION=$WXDIR$RELEASEBUILDDIR
+SOURCELOCATION=$HOME$RELEASEBUILDDIR
 ALTERNATESOURCELOCATION=""
 REQUIRES=""
 PROVIDES="buildgtku"
@@ -60,6 +59,7 @@ case $# in
           ABORT="TRUE"
       else
           echo "  wxWidgets will be built for the $1 version only"
+          echo "  wxWidgets will be built base path: $WXBASEDIR"
           BUILDS="$1"
       fi
         ;;
@@ -69,27 +69,27 @@ case $# in
           echo "  Unrecognized first parameter $1 Use \"debug\" \"release\" or \"debug release\""
           ABORT="TRUE"
       else
-        echo "  wxWidgets is specified to be built as a \"$1\" configuration"
+        echo "  wxWidgets is specified to be built for the \"$1\" configuration(s)"
         # Abort unless path in $2 parameter is writeable
         ABORT="TRUE"
-        WXSOURCETREE=$2
-        # The following 2 lines replace any initial ~/ on the $WXSOURCETREE path received from $2
-        result_string="${WXSOURCETREE/~\//$HOME/}"
-        WXSOURCETREE=$result_string
-        # The following 2 lines remove any final / on the WXSOURCETREE path
-        result_string=${WXSOURCETREE%/}
-        WXSOURCETREE=$result_string
-        echo "  wxWidgets location specified to be at: $WXSOURCETREE/$WXDIRNAME"
-        #echo "WXSOURCETREE is: $WXSOURCETREE"
-        mkdir -p $WXSOURCETREE
-        # The following touch ... line makes ABORT="FALSE" if $WXSOURCETREE is writable
-        # and if $WXSOURCETREE is not writeable it suppresses the error message
-        touch "${WXSOURCETREE}/foo" 2>/dev/null && rm -f "${WXSOURCETREE}/foo" && export ABORT="FALSE"
+        WXBASEDIR=$2
+        # The following 2 lines replace any initial ~/ on the $WXBASEDIR path received from $2
+        result_string="${WXBASEDIR/~\//$HOME/}"
+        WXBASEDIR=$result_string
+        # The following 2 lines remove any final / on the WXBASEDIR path
+        result_string=${WXBASEDIR%/}
+        WXBASEDIR=$result_string
+        echo "  wxWidgets location base path is: $WXBASEDIR"
+        #echo "WXBASEDIR is: $WXBASEDIR"
+        mkdir -p $WXBASEDIR
+        # The following touch ... line makes ABORT="FALSE" if $WXBASEDIR is writable
+        # and if $WXBASEDIR is not writeable it suppresses the error message
+        touch "${WXBASEDIR}/foo" 2>/dev/null && rm -f "${WXBASEDIR}/foo" && export ABORT="FALSE"
         #echo "ABORT is: $ABORT"
         if [ x"$ABORT" = x"TRUE" ]; then
-            echo "***Cannot write to path: $WXSOURCETREE"
+            echo "***Cannot write to path: $WXBASEDIR"
         else
-            echo "The base path: $WXSOURCETREE appears to be writeable"
+            echo "The base path: $WXBASEDIR appears to be writeable"
         fi
       fi
       BUILDS="$1"
@@ -155,21 +155,18 @@ fi
 #echo "12 - Conflicts: [ $CONFLICTS ]"
 #echo "13 - Replaces: [ $REPLACES ]"
 
-cd $WXSOURCETREE
-LASTERRORLEVEL=$?
-if [ $LASTERRORLEVEL != 0 ]; then
-  echo -e "\n***Could not access location: $WXSOURCETREE Aborting..."
-  exit $LASTERRORLEVEL
-fi
-
-# Check for existing wxWidgets dir, download and builds at $WXSOURCETREE
-# Create $WXSOURCETREE/$WXDIRNAME if it doesn't already exist
-mkdir -p $WXSOURCETREE/$WXDIRNAME
-if [ -d $WXSOURCETREE/$WXDIRNAME ]; then
-  echo "Found the \"$WXDIRNAME\" directory at the base path"
+# Check for existing wxWidgets dir, download and builds at $WXBASEDIR
+# Create $WXBASEDIR/$WXDIRNAME if it doesn't already exist
+if ! [ -d $WXBASEDIR/$WXDIRNAME ]; then
+  echo "Creating $WXDIRNAME folder at: $WXBASEDIR"
+  mkdir -p $WXBASEDIR/$WXDIRNAME
+  if [ $? -ne 0 ]; then
+    echo "***Could not create the $WXBASEDIR/$WXDIRNAME directory."
+    echo "Aborting..."
+    exit 1
+  fi 
 else
-  echo "***Could not create the $WXSOURCETREE/$WXDIRNAME directory. Aborting..."
-  exit 1
+  echo "Found the \"$WXDIRNAME\" directory at the base path"
 fi 
 SKIPRELEASEBUILD="FALSE"
 SKIPDEBUGBUILD="FALSE"
@@ -177,67 +174,78 @@ WXARCHIVEFOUND="FALSE"
 # If the static wx lib already exists for the current build (release or debug), 
 # we can skip the configuring and build steps
 # Check for a release version of the library libwx_gtk3u-3.0.a at:
-# $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR/lib/libwx_gtk3u-3.0.a
-if [ -f $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR/lib/libwx_gtk3u-3.0.a ]; then
+# $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR/lib/libwx_gtk3u-3.0.a
+if [ -f $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR/lib/libwx_gtk3u-3.0.a ]; then
   SKIPRELEASEBUILD="TRUE"
-  echo "  A release static build of the wx library already exists at the specified path"
+  if [ x"$1" = x"release" ]; then
+    echo "  A release static build of the wx library already exists at the specified path"
+    echo "    Nothing to be done for the release configuration!"
+  fi
 else
   if [ x"$1" = x"release" ]; then
     echo "   ***A release static build of the wx library was not found there"
   fi
 fi
 # Check for a debug version of the library libwx_gtk3u-3.0.a at:
-# $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR/lib/libwx_gtk3u-3.0.a
-if [ -f $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR/lib/libwx_gtk3u-3.0.a ]; then
+# $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR/lib/libwx_gtk3u-3.0.a
+if [ -f $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR/lib/libwx_gtk3u-3.0.a ]; then
   SKIPDEBUGBUILD="TRUE"
-  echo "  A debug static build of the wx library already exists at the specified path"
+  if [ x"$1" = x"debug" ]; then
+    echo "  A debug static build of the wx library already exists at the specified path"
+    echo "    Nothing to be done for the debug configuration!"
+  fi
 else
   if [ x"$1" = x"debug" ]; then
     echo -e "\n***A debug static build of the wx library was not found there"
   fi
 fi
 
-echo "Script specifies building wxWidgets for a \"$1\" configuration"
+if [ x"$SKIPDEBUGBUILD" = x"FALSE" ] || [ x"$SKIPRELEASEBUILD" = x"FALSE" ] ; then
+  # A build will be necessary for either release or debug configuration.
+  # Check for essential packages needed to build wxWidgets
+  # The configure call will fail if the build tools are not installed,
+  # so check if build-essential package is installed, and if not install it.
+  echo "Checking for essential packages..."
+  PkgInstalled=$(dpkg-query -W --showformat='${Status}\n' build-essential | grep "install ok installed")
+  if [ "x$PkgInstalled" == x"" ]; then
+    # The sudo command below should not be executed when the script is called from CodeBlocks
+    # since it must execute without input from the user. 
+    #echo -e "\nInstalling the build-essential package which is needed to build wxWidgets..."
+    #sudo apt-get -y install build-essential
+    echo "***You must install the build-essential package to build wxWidgets"
+    echo "***Suggested command: sudo apt-get install build-essential"
+    ABORT="TRUE"
+  else
+    echo "  The build-essential package is already installed"
+  fi
+  # The configure call will fail if the GTK+ development library is not installed,
+  # so check if libgtk-3-dev package is installed, and if not install it.
+  PkgInstalled=$(dpkg-query -W --showformat='${Status}\n' libgtk-3-dev | grep "install ok installed")
+  if [ "x$PkgInstalled" == x"" ]; then
+    # The sudo command below should not be executed when the script is called from CodeBlocks
+    # since it must execute without input from the user. 
+    #echo -e "\nInstalling the libgtk-3-dev package which is needed to build wxWidgets..."
+    #sudo apt-get -y install libgtk-3-dev
+    echo "***You must install the libgtk-3-dev package to build wxWidgets"
+    echo "***Suggested command: sudo apt-get install libgtk-3-dev"
+    ABORT="TRUE"
+  else
+    echo "  The libgtk-3-dev package is already installed"
+  fi
+  if [ x"$ABORT" = x"TRUE" ]; then
+    exit 1
+  fi 
+  # If we get to here $ABORT is "FALSE"
+fi
+
 # Check for an existing copy of the $WXGZARCHIVE
-if [ -f $WXSOURCETREE/$WXGZARCHIVE ]; then
+if [ -f $WXBASEDIR/$WXGZARCHIVE ]; then
+  echo "The $WXGZARCHIVE archive exists at: $WXBASEDIR"
   WXARCHIVEFOUND="TRUE"
 else
   # The $WXARCHIVEFOUND archive was not found, so see if we need it for a build
   if [ x"$SKIPDEBUGBUILD" = x"FALSE" ] || [ x"$SKIPRELEASEBUILD" = x"FALSE" ] ; then
-    # Check for essential packages needed to build wxWidgets
-    # The configure call will fail if the build tools are not installed,
-    # so check if build-essential package is installed, and if not install it.
-    echo "Checking for essential packages..."
-    PkgInstalled=$(dpkg-query -W --showformat='${Status}\n' build-essential | grep "install ok installed")
-    if [ "x$PkgInstalled" == x"" ]; then
-      # The sudo command below should not be executed when the script is called from CodeBlocks
-      # since it must execute without input from the user. 
-      #echo -e "\nInstalling the build-essential package which is needed to build wxWidgets..."
-      #sudo apt-get -y install build-essential
-      echo -e "\n***You must install the build-essential package to build wxWidgets"
-      ABORT="TRUE"
-    else
-      echo "  The build-essential package is already installed"
-    fi
-
-    # The configure call will fail if the GTK+ development library is not installed,
-    # so check if libgtk-3-dev package is installed, and if not install it.
-    PkgInstalled=$(dpkg-query -W --showformat='${Status}\n' libgtk-3-dev | grep "install ok installed")
-    if [ "x$PkgInstalled" == x"" ]; then
-      # The sudo command below should not be executed when the script is called from CodeBlocks
-      # since it must execute without input from the user. 
-      #echo -e "\nInstalling the libgtk-3-dev package which is needed to build wxWidgets..."
-      #sudo apt-get -y install libgtk-3-dev
-      echo -e "\n***You must install the libgtk-3-dev package to build wxWidgets"
-      ABORT="TRUE"
-    else
-      echo "  The libgtk-3-dev package is already installed"
-    fi
-    if [ x"$ABORT" = x"TRUE" ]; then
-      exit 1
-    fi 
-    # If we get to here $ABORT is "FALSE"
-  
+    # A build will be necessary for either release or debug configuration.
     # Need to download the $WXGZARCHIVE
     # Test for Internet connection
     echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
@@ -250,15 +258,15 @@ else
     fi
     echo -e "\nRetrieving the $WXGZARCHIVE archive from:"
     echo    "   $WX302URL..."
-    echo    "Downloading $WXGZARCHIVE to $WXSOURCETREE/$WXDIRNAME"
-    cd $WXSOURCETREE/$WXDIRNAME
+    echo    "Downloading $WXGZARCHIVE to $WXBASEDIR"
+    cd $WXBASEDIR
     wget --no-clobber --no-directories $WX302URL
     if [ $? -ne 0 ]
     then
       echo "***Unable to download $WXGZARCHIVE: wget error: $?"
       exit 1
     fi
-    echo -e "\nExpanding the $WXGZARCHIVE archive"
+    echo "Expanding the $WXGZARCHIVE archive"
     # Extract wxWidgets from the .gz archive
     # can include v option to tar below for verbose output
     tar xjf $WXGZARCHIVE
@@ -272,18 +280,18 @@ for i in $BUILDS; do
       echo      "**  Configuring the Debug version... **"
       echo      "***************************************"
       sleep 1
-      if [ -d $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR ]; then
-        echo "   Removing stale $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR dir"
-        rm -rf $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR
+      if [ -d $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR ]; then
+        echo "   Removing stale $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR dir"
+        rm -rf $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR
       else
-        mkdir -p $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR
+        mkdir -p $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR
       fi
-      cd $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR
-      echo -e "\nConfiguring the $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR with options: "
+      cd $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR
+      echo -e "\nConfiguring the $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR with options: "
       echo "   $DEBUGCONFIGOPTIONS"
       sleep 3
       ../configure $DEBUGCONFIGOPTIONS
-      echo -e "\nClean the $WXSOURCETREE/$WXDIRNAME$DEBUGBUILDDIR library..."
+      echo -e "\nClean the $WXBASEDIR/$WXDIRNAME$DEBUGBUILDDIR library..."
       sleep 3
       make clean
       echo -e "\n************************************"
@@ -302,18 +310,18 @@ for i in $BUILDS; do
       echo      "**  Configuring the Release version... **"
       echo      "*****************************************"
       sleep 1
-      if [ -d $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR ]; then
-        echo "   Removing stale $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR dir"
-        rm -rf $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR
+      if [ -d $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR ]; then
+        echo "   Removing stale $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR dir"
+        rm -rf $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR
       else
-        mkdir -p $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR
+        mkdir -p $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR
       fi
-      cd $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR
-      echo -e "\nConfiguring the $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR with options: "
+      cd $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR
+      echo -e "\nConfiguring the $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR with options: "
       echo "   $RELEASECONFIGOPTIONS"
       sleep 3
       ../configure $RELEASECONFIGOPTIONS
-      echo -e "\nClean the $WXSOURCETREE/$WXDIRNAME$RELEASEBUILDDIR library..."
+      echo -e "\nClean the $WXBASEDIR/$WXDIRNAME$RELEASEBUILDDIR library..."
       sleep 3
       make clean
       echo -e "\n**************************************"
