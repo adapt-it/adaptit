3c2c2a1ed6711ae1d130010d4df8529492213ff9
pngbill <bill_martin@sil.org>
2017-03-20 08:42:09 -0500

    Change Pre-Release date to 20 March 2017.

--- source/Adapt_It.h
+++ source/Adapt_It.h
@@ -211,7 +211,7 @@ class KBSharingMgrTabbedDlg;
 #define VERSION_BUILD_PART 2 // DO NOT CHANGE UNTIL YOU READ THE ABOVE NOTE AND COMMENTS !!!
 #define VERSION_REVISION_PART ${svnversion}
 #define PRE_RELEASE 1  // set to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
-#define VERSION_DATE_DAY 16
+#define VERSION_DATE_DAY 20
 #define VERSION_DATE_MONTH 3
 #define VERSION_DATE_YEAR 2017
 const wxString appVerStr(_T("6.8.2"));
