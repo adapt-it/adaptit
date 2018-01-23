5296d7b21e98df5c6f087b4739375c29b7d49fbd
pngbill <bill_martin@sil.org>
2018-01-22 20:18:25 -0600

    Revised the functions: ParatextVersionInstalled(), GetParatextInstallDirPath(), and GetParatextProjectsDirPath() with fall-back code in the event that the wxRegKey calls to the Windows registry fail to provide the PT info about its installation dir path and its projects dir path. If the registry calls return empty path strings or the path strings don't exist, the fall-back code attempts to determine if the usual default paths exist and are valid, and if so, these functions return those default values.

--- source/Adapt_It.h
+++ source/Adapt_It.h
@@ -34,8 +34,8 @@
 // When Use_in_line_Choose_Translation_DropDown is defined the default behavior is 
 // to present multiple translation equivalents in a drop-down combobox aligned to 
 // the bottom of the phrasebox instead of the legacy code way of showing a modal 
-// ChooseTranslation dialog.
-//#define Use_in_line_Choose_Translation_DropDown
+// ChooseTranslation dialog. Comment out to only use the Choose Translation dialog.
+#define Use_in_line_Choose_Translation_DropDown
 
 //#define AUTHENTICATE_AS_BRUCE
 
