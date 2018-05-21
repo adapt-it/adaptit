5cf1adf83bce913292ffc6cf7be91859518a6b1a
pngbill <bill_martin@sil.org>
2018-05-21 09:00:09 -0500

    Commented out bool bDeleted line in LogDropdownState() to avoid gcc warning of unused variable.

--- source/Adapt_It.cpp
+++ source/Adapt_It.cpp
@@ -54121,8 +54121,8 @@ void CAdapt_ItApp::LogDropdownState(wxString functionName, wxString fileName, in
 					pRefStr = (CRefString*)tpos->GetData();
 					wxASSERT(pRefStr != NULL);
 					tpos = tpos->GetNext();
-					bool bDeleted = pRefStr->GetDeletedFlag();
-//					if (!bDeleted)   // uncomment out if these are wanted
+//					bool bDeleted = pRefStr->GetDeletedFlag();   // uncomment out if these are wanted
+//					if (!bDeleted)
 //					{
 //						msg = _T("KB-stored translations for key:  %s  , CRefString's m_translation is:  %s");
 //						msg = msg.Format(msg, pActiveSrcPhrase->m_key.c_str(), pRefStr->m_translation.c_str());
