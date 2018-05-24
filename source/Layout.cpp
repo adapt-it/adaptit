54ec85cc1fb4ba961cb478c8008aed953781ef20
pngbill <bill_martin@sil.org>
2018-05-23 22:32:50 -0500

    Added an m_pTargetBox->SetSelection(-1, -1) call after BEW's code modifications which called ChangeValue(). The SetSelection(-1, -1) call is needed for the Linux version after the ChangeValue() call, otherwise the Linux version loses the selection of the phrasebox contents.

--- source/Layout.cpp
+++ source/Layout.cpp
@@ -988,7 +988,8 @@ void CLayout::PlaceBox()
 				}
 			}
 			m_pApp->m_pTargetBox->ChangeValue(m_pApp->m_targetPhrase); // keep m_pTargetBox contents in sync with m_targetPhrase
-		}
+            m_pApp->m_pTargetBox->SetSelection(-1, -1); // whm added 23May2018 otherwise Linux version looses selection of text in phrasebox
+        }
 #if defined (_DEBUG) && defined (_ABANDONABLE)
 		m_pApp->LogDropdownState(_T("PlaceBox() after call and return from SetupDropDownPhraseBoxForThisLocation()"), _T("Layout.cpp"), 985);
 #endif
