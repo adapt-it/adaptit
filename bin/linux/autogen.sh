3c78c3a188bff20d84870d6e12e69e8a13af8f0a
pngbill <bill_martin@sil.org>
2018-07-29 18:12:01 -0500

    Revert the bin/linux/autogen.sh, the po/Makefile.in.in and all /m4/ dir files to what they were in the external repository. This is attempt to get a good build both locally (against gettext 0.19) as well as in TravisCI which uses gettext 0.18.

--- bin/linux/autogen.sh
+++ bin/linux/autogen.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-# whm 29Jul2018 replaced old autogen.sh with single line autoreconf -fi below:
-# autoreconf options: -f force creation of all autotool helpers, -i copy missing ones
-# Output versions
-#autoreconf --version
-aclocal --version
-libtoolize --version
-automake --version
-autoconf --version
-gettext --version
-autoreconf -fi
-
-# Original autogen.sh commands below:
 LIBTOOLIZE=libtoolize
 if [ "`uname`" = "Darwin" ] ; then
    LIBTOOLIZE=glibtoolize
