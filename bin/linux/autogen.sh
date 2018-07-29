7d1e1ee3233b067565ef1331dccf961a2b4c2423
pngbill <bill_martin@sil.org>
2018-07-29 17:12:58 -0500

    In autogen.sh try calling both autoreconf -fi and afterwards also calling each individual call of aclocal, libtoolize, automake, and autoconf.

--- bin/linux/autogen.sh
+++ bin/linux/autogen.sh
@@ -11,16 +11,16 @@ gettext --version
 autoreconf -fi
 
 # Original autogen.sh commands below:
-#LIBTOOLIZE=libtoolize
-#if [ "`uname`" = "Darwin" ] ; then
-#   LIBTOOLIZE=glibtoolize
-#fi
-## call aclocal to generate an m4 environment for autotools to use
-#aclocal -I m4
-#$LIBTOOLIZE --automake --force --copy
-##libtoolize --automake --force --copy
-## call automake to turn Makefile.am into a Makefile.in
-#automake -v -a -c -Wno-portability
-## call autoconf to turn configure.ac into a configure script (which when invoked generates Makefile from Makefile.in)
-#autoconf
+LIBTOOLIZE=libtoolize
+if [ "`uname`" = "Darwin" ] ; then
+   LIBTOOLIZE=glibtoolize
+fi
+# call aclocal to generate an m4 environment for autotools to use
+aclocal -I m4
+$LIBTOOLIZE --automake --force --copy
+#libtoolize --automake --force --copy
+# call automake to turn Makefile.am into a Makefile.in
+automake -v -a -c -Wno-portability
+# call autoconf to turn configure.ac into a configure script (which when invoked generates Makefile from Makefile.in)
+autoconf
 
