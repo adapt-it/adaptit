3f3aa524cdf43d95facd1ddbdaf6948ca08ce749
pngbill <bill_martin@sil.org>
2018-07-29 14:38:03 -0500

    Modify the autogen.sh script to only call autoreconf -fi which forces replacement of all autotools and copying of missing files.

--- bin/linux/autogen.sh
+++ bin/linux/autogen.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-LIBTOOLIZE=libtoolize
-if [ "`uname`" = "Darwin" ] ; then
-   LIBTOOLIZE=glibtoolize
-fi
-# call aclocal to generate an m4 environment for autotools to use
-aclocal -I m4
-$LIBTOOLIZE --automake --force --copy
-#libtoolize --automake --force --copy
-# call automake to turn Makefile.am into a Makefile.in
-automake -v -a -c -Wno-portability
-# call autoconf to turn configure.ac into a configure script (which when invoked generates Makefile from Makefile.in)
-autoconf
+# whm 29Jul2018 replaced old autogen.sh with single line autoreconf -fi below:
+# autoreconf options: -f force creation of all autotool helpers, -i copy missing ones
+autoreconf -fi
+
+# Original autogen.sh commands below:
+#LIBTOOLIZE=libtoolize
+#if [ "`uname`" = "Darwin" ] ; then
+#   LIBTOOLIZE=glibtoolize
+#fi
+## call aclocal to generate an m4 environment for autotools to use
+#aclocal -I m4
+#$LIBTOOLIZE --automake --force --copy
+##libtoolize --automake --force --copy
+## call automake to turn Makefile.am into a Makefile.in
+#automake -v -a -c -Wno-portability
+## call autoconf to turn configure.ac into a configure script (which when invoked generates Makefile from Makefile.in)
+#autoconf
 
