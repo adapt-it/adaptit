fc6becd60ea364a75f2583c2aff19af2274de2bc
pngbill <bill_martin@sil.org>
2020-04-29 21:23:26 -0500

    Remove extraneous # from source #ifdef header - cause build failure on TravisCI

--- res/vectorized/insplaceholder_right_16.cpp
+++ res/vectorized/insplaceholder_right_16.cpp
@@ -1,4 +1,4 @@
-##ifndef INSPLACEHOLDER_RIGHT_PNG_16_H
+#ifndef INSPLACEHOLDER_RIGHT_PNG_16_H
 #define INSPLACEHOLDER_RIGHT_PNG_16_H
 static const unsigned char insplaceholder_right_png_16[] = {
 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d, 
