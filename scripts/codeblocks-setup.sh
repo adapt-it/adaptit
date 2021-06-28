aff4f451cd96a44c2d62e5946e8588b0eeebe6aa
pngbill <bill_martin@sil.org>
2021-06-28 14:17:58 -0500

    Revision of the CodeBlocks adaptit.cbp for Linux project file to include additional configurations for building Adapt It on Linux. Revision of the codeblocks-setup.sh script for installing CodeBlocks on Linux developer system. Added the Setting up wxWidgets and Adapt It on Ubuntu Linux.odt document to the platform_setup_docs directory. This document has extensive revisions and describes the new configurations in CodeBlocks. It replaces the old document of same name but with rtf extension.

--- scripts/codeblocks-setup.sh
+++ scripts/codeblocks-setup.sh
@@ -5,6 +5,11 @@
 # Author: Bill Martin <bill_martin@sil.org>
 # Date: 2017-01-26
 # Requires an Internet connection to use apt-get install
+# whm 22Jun2021 revised to add recent distros of Ubuntu and LinuxMint to 
+#   the supportedCodenames variable and the case $distCodename in [case statement]. 
+#   Removed sed routines for updating /etc/apt/sources... and used the more efficient
+#   sudo add-apt-repository ppa:codeblocks-devs/release call instead.
+#   Also updated the Codeblocks CBSIGNKEY and its PGP Public key block.
 
 # Test for Internet connection
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
@@ -16,18 +21,16 @@ else
     exit 1
 fi
 
-#
-APT_SOURCES_LIST_DIR="/etc/apt/sources.list"
 supportedDistIDs="LinuxMint Ubuntu"
-supportedCodenames="maya qiana rebecca rafaela rosa sarah serena sonya sylvia tara precise trusty utopic vivid wily xenial yakkety zesty artful bionic"
+supportedCodenames="maya qiana rebecca rafaela rosa sarah serena sonya sylvia tara tessa tina tricia ulyana ulyssa uma precise trusty utopic vivid wily xenial yakkety zesty artful bionic focal"
 echo -e "\nDetermine if system is LinuxMint or Ubuntu and its Codename"
 # Determine whether we are setting up a LinuxMint/Wasta system or a straight Ubuntu system
 # The 'lsb_release -is' command returns "LinuxMint" on Mint systems and "Ubuntu" on Ubuntu systems.
 distID=`lsb_release -is`
 echo "  This system is: $distID"
 # Determine what the Codename is of the system
-# The 'lsb_release -cs' command returns "maya", "rafaela", "rosa", "sarah", "serena" on Mint LTS systems, 
-#   and "precise", "trusty", "utopic", "vivid", "wily", "xenial", "yakkety" on Ubuntu systems"
+# The 'lsb_release -cs' command returns "tara", "tessa", "tina", "tricia", "ulyana", "ulyssa", "uma", on Mint LTS systems, 
+#   and "trusty", "xenial", "bionic", "focal", on Ubuntu systems.
 distCodename=`lsb_release -cs`
 echo "  The Codename is: $distCodename"
 if echo "$supportedDistIDs" | grep -q "$distID"; then
@@ -37,6 +40,7 @@ if echo "$supportedDistIDs" | grep -q "$distID"; then
   else
     echo "But this script does not support setup on $distID $distCodename"
     echo "Aborting..."
+    exit 1
   fi
 else
   echo "This script does not support setup on $distID"
@@ -44,79 +48,21 @@ else
   exit 1
 fi
 
-# Get the current Codename.
-case $distCodename in
-  "maya")
-  distCodename="precise"
-  ;;
-  "qiana")
-  distCodename="trusty"
-  ;;
-  "rebecca")
-  distCodename="trusty"
-  ;;
-  "rafaela")
-  distCodename="trusty"
-  ;;
-  "rosa")
-  distCodename="trusty"
-  ;;
-  "sarah")
-  distCodename="xenial"
-  ;;
-  "serena")
-  distCodename="xenial"
-  ;;
-  "sonya")
-  distCodename="xenial"
-  ;;
-  "sylvia")
-  distCodename="xenial"
-  ;;
-  "tara")
-  distCodename="bionic"
-  ;;
-esac
-echo "  The Modified Codename for Deveopment is: $distCodename"
-
-
-
-# Setup CodeBlocks
-echo "Seting up CodeBlocks..."
-
-
 # ***************************************************************************
 # Install CodeBlocks PPA Repository and Authorization Key
 # ***************************************************************************
+# Setup CodeBlocks
+echo "calling add-apt-repository ppa:codeblocks-devs/release ..."
+sudo add-apt-repository ppa:codeblocks-devs/release
+sudo apt-get update
 
-# Add CodeBlocks repository
-echo -e "\nAdding CodeBlocks PPA repository to software sources"
-case $distCodename in
-  "precise")
-    # 
-    sudo sed -i -e '$a deb http://ppa.launchpad.net/damien-moore/codeblocks-stable/ubuntu precise main' \
-        -i -e '\@deb http://ppa.launchpad.net/damien-moore/codeblocks-stable/ubuntu precise main@d' \
-        $APT_SOURCES_LIST_DIR
-  ;;
-  "trusty")
-    # 
-    sudo sed -i -e '$a deb http://ppa.launchpad.net/damien-moore/codeblocks-stable/ubuntu trusty main' \
-        -i -e '\@deb http://ppa.launchpad.net/damien-moore/codeblocks-stable/ubuntu trusty main@d' \
-        $APT_SOURCES_LIST_DIR
-  ;;
-  "xenial")
-    # 
-    sudo sed -i -e '$a deb http://ppa.launchpad.net/damien-moore/codeblocks-stable/ubuntu xenial main' \
-        -i -e '\@deb http://ppa.launchpad.net/damien-moore/codeblocks-stable/ubuntu xenial main@d' \
-        $APT_SOURCES_LIST_DIR
-  ;;
-esac
 
 # Ensure CodeBlocks key is installed
 # Note: The CodeBlocks key seems difficult to retrieve from its ppa.launchpad.net location,
 # so we'll generate it from a heredoc and not bother with getting the key from the Internet.
 echo -e "\nEnsuring the CodeBlocks key is installed for ppa.launchpad.net repository..."
-CBSIGNKEY="C99E40E1"
+# CBSIGNKEY="C99E40E1"
+CBSIGNKEY="F6B68557"
 CBKey=`apt-key list | grep $CBSIGNKEY`
 if [ -z "$CBKey" ]; then
   echo "The CodeBlocks key is NOT installed."
@@ -125,18 +71,36 @@ if [ -z "$CBKey" ]; then
   TEMPFILE=$TEMPDIR/cb.gpg
 cat <<EOF >$TEMPFILE
 -----BEGIN PGP PUBLIC KEY BLOCK-----
-Version: SKS 1.1.5
-Comment: Hostname: keyserver.ubuntu.com
+Comment: Hostname: 
+Version: Hockeypuck ~unreleased
 
-mI0ESveZFQEEAKThHGzw+SstcL3VGb77mygRPQHUmw99SHUWU+rNxeAfZh8Nrk6XLaIqHFxV
-5eBUuFCVlafPElLIqzYnRc+/fcnp1ULEXcBYrCOnroHWR0zcdgbSAIhQaRYe8hXTuGKp/zXa
-SsgXqeX4Q8QaYLx7oJYX2MTLLrtbo+p6M+AnNgItABEBAAG0GExhdW5jaHBhZCBQUEEgZm9y
-IHNwaWxseoi2BBMBAgAgBQJK95kVAhsDBgsJCAcDAgQVAggDBBYCAwECHgECF4AACgkQh7sH
-tMmeQOFZywQAorsav42L8CCfEtdsMIHsGZxkYLfY2FRkEzyXAneIWIFwTeibxbjIcxYg4NzA
-oEoDJ1//6+YRAoqooBRGQSdrTYMfkokl4Rr+/98hV1LPCO8TSVTmvmeCIBsENB32Pn1zEbYC
-vqNpsgdbrHxLhtNf+bZIO1sfpslepi6o8B9u0jU=
-=DYMr
+xsFNBFeyJAABEADU01CIvATGBQCZcZRLQNX7sVkTw1snGx1XW18Z/61PlNVJ5vCV
+QNORuya3uWCWDAL7Ok4XExgfmFEW8O5FgjRFlyyJTQOJZiQ/MYw5Y1b0NqO/snnj
+4EDIdclzHnL+BunsGXn1VoS4ae26fdLUBU2XRY+Rd/DEtGnSNviLs+TSm3uKIOSI
+nyApiCSNYt8oecjrvUNJ9R5aEztunY6XxI4sgsnfsLIqNf9p/SBHAffmrNJuOL6y
+oRt89zNh8AAzERxIfCLOl/wY7vx5Xe8RC3V5gSa1iRBvRX/dNKIuWF2CI30UTC/x
+ymRo1cQpPk1zAy30Beb13HNl6gN42EKOv54elej89Gs2IveM9COWAQmMaYVIdKMY
+W6H8Rx8xgeZ+aUMHSR7+CHyFtRMGAoNnRrKD2XBnxmtCbTviJqF6pE43zDbFhpjg
+5DoJtCjyWOgnkipk09DENdLNpnyNyGUj2U0Fj3QL4fUWUqXWavzkOKQE9KpVUH4h
+/XBlizL+dtA55woQlERSp+58+DsDN+GiKNZNEOPcwVkEZnRS1zZKQqpUHdoJZ1Ny
+VdPGroRMBuMQLwHGI5Pikk3SZfNuuaRq9h9kilmoI+lnsL/kxElAk/jPoMxk2j/+
+SHgfJK3v5YasDrmU1P+5S3KStP9wjuiOeNw1ITGPp4+E4Jiy9H3O/Y7qtQARAQAB
+zSlMYXVuY2hwYWQgUFBBIGZvciBDb2RlOjpCbG9ja3MgRGV2ZWxvcGVyc8LBeAQT
+AQIAIgUCV7IkAAIbAwYLCQgHAwIGFQgCCQoLBBYCAwECHgECF4AACgkQ9ujmeva2
+hVfGhw//czgVP7yPtkHfs69MGd2XlHHMoHOIp6/l+6BUnTMxomvU0hBXjKKDdZAe
+CQcYJZ6WcBzeXt+BR/3m4XVrpvG58b6WXyK+CGin0JCRYji1fkGsZfVHfbdNdnrw
+NWG/qh8T262aRne/QYSvU+En4FifZcdi7k0ZgQxdNxpCiMQNb6iO34sDujbAoiiC
+EOGsLYQxqG+GXcE+2qP3C/mML1NyeLHY0pshM/lFbx0JRynUi3Wqq75yHIywnHFg
+dMJiZFFSU7bFX8/A35h5/CxxrkAHgCQ+Lg0IZ6i29bggVRobQz5tmvy8nhr03lGC
+8nhBSmPtxFe57ce475Rw+itMLjLM02MFL0pEhpNPpJVOVP/G6P4gK4HYlxwwKuLi
+Ngaf44YO9HBEBT0ihX504sBxG5wHZ73HMAsvi42rOBFqM3eT7Mh07+ipUlQgp/dX
+SmkIHff7BYvGyePsc3LJws2WQTiQtbI/AXWUzwNOAKWyAUVeiJslDYEWWSqNQrSt
+5L+enM4cHo+wQ/6TFXEfVh1YFt0c6B1B16lVt3hpTBRvuE6HRLHyGWiYiUsepeZL
+uIhlq3T3D0BBQDCsvtruK92afrqg+YSupB0U+ot15Ij4dt0YZ9pTSAIFeuHKv4gH
+jYfuR04qtIubHtYnHKimEBxyF62I1PO/praF15eSbIyg4bxKg9k=
+=SZDD
 -----END PGP PUBLIC KEY BLOCK-----
+
 EOF
 
   if [ -f $TEMPFILE ]; then
@@ -158,11 +122,9 @@ fi
 echo -e "\nRefresh apt lists via apt-get update"
 sudo apt-get -q update
 
-exit 0
-
-# Install the latest CodeBlocks (17.12) package
+# Install the latest CodeBlocks package
 echo -e "\nInstalling the CodeBlocks package..."
-sudo apt-get install codeblocks -y
+sudo apt-get install codeblocks codeblocks-contrib -y
 
 LASTERRORLEVEL=$?
 if [ $LASTERRORLEVEL != 0 ]; then
