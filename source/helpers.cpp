151600f2a62f864f05ac2ce1778b052de6ecc2a5
adaptitbruce <bruce_waters@sil.org>
2022-05-09 12:29:40 +1000

    commented out my line 88 in helpers.cpp - a hard coded win path that breaks non-win builds

--- source/helpers.cpp
+++ source/helpers.cpp
@@ -85,7 +85,7 @@
 // these extra includes, for wchar_t support, wxChar, & wxString
 #include <string.h>
 #include <time.h>
-#include "C:\Program Files (x86)\MariaDB 10.5\include\mysql\mysql.h"
+//#include "C:\Program Files (x86)\MariaDB 10.5\include\mysql\mysql.h"  // BEW commented out 9May22
 // and put the directory path to MariaDB 10.5's libmariadb.lib object file library into the Additional Library Directories
 // for both Unicode Debug build, and Unicode Release build: the path is: C:\Program Files (x86)\MariaDB 10.5\lib -- I've done that 18Apr22
 //#endif
