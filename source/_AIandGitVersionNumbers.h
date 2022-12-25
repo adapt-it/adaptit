d5235c6eeb8eeb4cd114f57edc545be9b31ada79
pngbill <bill_martin@sil.org>
2022-12-24 18:04:22 -0600

    Changes version number to 6.11.0 and tentative release date to 5 January 2023. Updates copyright dates to 2023. Updates document release numbers and dates. Updates the po and mo localizations to current code updating the tpi Tok Pisin translations. Updates copyright date and comments in Inno Setup Scripts.

--- source/_AIandGitVersionNumbers.h
+++ source/_AIandGitVersionNumbers.h
@@ -42,19 +42,19 @@
 
 // when changing these string values make sure to also change the corresponding int values below
 #define AI_VERSION_MAJOR_STR "6"
-#define AI_VERSION_MINOR_STR "10"
-#define AI_VERSION_BUILD_PART_STR "7"
+#define AI_VERSION_MINOR_STR "11"
+#define AI_VERSION_BUILD_PART_STR "0"
 #define AI_VERSION_MAJOR 6
-#define AI_VERSION_MINOR 10
-#define AI_VERSION_BUILD_PART 7
+#define AI_VERSION_MINOR 11
+#define AI_VERSION_BUILD_PART 0
 
 // when changing these string values make sure to also change the corresponding int values below
-#define VERSION_DATE_DAY_STR "20"
-#define VERSION_DATE_MONTH_STR "5"
-#define VERSION_DATE_YEAR_STR "2022"
-#define VERSION_DATE_DAY 20
-#define VERSION_DATE_MONTH 5
-#define VERSION_DATE_YEAR 2022
+#define VERSION_DATE_DAY_STR "5"
+#define VERSION_DATE_MONTH_STR "1"
+#define VERSION_DATE_YEAR_STR "2023"
+#define VERSION_DATE_DAY 5
+#define VERSION_DATE_MONTH 1
+#define VERSION_DATE_YEAR 2023
 
 // set PRE_RELEASE define below to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
 #define PRE_RELEASE 0 
