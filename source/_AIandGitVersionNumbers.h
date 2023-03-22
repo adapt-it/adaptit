fc6614cc3360f8b148ed89e49056dfed08752dc6
pngbill <bill_martin@sil.org>
2023-03-22 14:20:20 -0500

    Updated the debian/changelog file with current version 6.11.0 and release date of 22Mar2023. Also updated the About Dialog's default version/date settings. Updated the _AIandGitVersionNumbers.h header with the new release date.

--- source/_AIandGitVersionNumbers.h
+++ source/_AIandGitVersionNumbers.h
@@ -49,11 +49,11 @@
 #define AI_VERSION_BUILD_PART 0
 
 // when changing these string values make sure to also change the corresponding int values below
-#define VERSION_DATE_DAY_STR "5"
-#define VERSION_DATE_MONTH_STR "1"
+#define VERSION_DATE_DAY_STR "21"
+#define VERSION_DATE_MONTH_STR "3"
 #define VERSION_DATE_YEAR_STR "2023"
-#define VERSION_DATE_DAY 5
-#define VERSION_DATE_MONTH 1
+#define VERSION_DATE_DAY 21
+#define VERSION_DATE_MONTH 3
 #define VERSION_DATE_YEAR 2023
 
 // set PRE_RELEASE define below to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
