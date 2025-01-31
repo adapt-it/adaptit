ff608e5eb9a6db846253c0d586d51cf23b9ba739
pngbill <bill_martin@sil.org>
2025-01-30 20:02:36 -0600

    Changed release date for version 6.11.5 to 30 January 2025. Changed copyright year to 2025 all in various doce.

--- source/_AIandGitVersionNumbers.h
+++ source/_AIandGitVersionNumbers.h
@@ -49,15 +49,15 @@
 #define AI_VERSION_BUILD_PART 5
 
 // when changing these string values make sure to also change the corresponding int values below
-#define VERSION_DATE_DAY_STR "11"
+#define VERSION_DATE_DAY_STR "30"
 #define VERSION_DATE_MONTH_STR "1"
 #define VERSION_DATE_YEAR_STR "2025"
-#define VERSION_DATE_DAY 11
+#define VERSION_DATE_DAY 30
 #define VERSION_DATE_MONTH 1
 #define VERSION_DATE_YEAR 2025
 
 // set PRE_RELEASE define below to 0 (zero) for normal releases; 1 to indicate "Pre-Release" in About Dialog
-#define PRE_RELEASE 1
+#define PRE_RELEASE 0
 
 #define AI_VERSION_DOT "."
 #define AI_VERSION_COMMA ","
